21bd25ecea893e1ef92ffcbb7a3d0b835ef856c8
Frédéric Lepied <flepied@mandriva.com>
1999-10-07 18:23:53 +0000

    * first version.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@27 9bc8b190-ac0f-0410-8968-dc7d1f502856

---
+++ SignatureCheck.py
@@ -0,0 +1,32 @@
+#############################################################################
+# File		: SignatureCheck.py
+# Package	: rpmlint
+# Author	: Frederic Lepied
+# Created on	: Thu Oct  7 17:06:14 1999
+# Version	: $Id$
+# Purpose	: check the presence of a PGP signature.
+#############################################################################
+
+import AbstractCheck
+import commands
+import re
+import sys
+
+class SignatureCheck(AbstractCheck.AbstractCheck):
+    pgp_regex=re.compile("pgp", re.IGNORECASE)
+    
+    def __init__(self):
+	AbstractCheck.AbstractCheck.__init__(self, "FilesCheck")
+
+    def check(self, pkg, verbose):
+	res=commands.getstatusoutput("rpm -K " + pkg.filename)
+	if not res or res[0] != 0:
+	    sys.stderr.write("error checking signature of " + pkg.filename + "\n")
+	else:
+	    if not SignatureCheck.pgp_regex.search(res[1]):
+		print "E:", pkg.name, "no-signature"
+	
+# Create an object to enable the auto registration of the test
+check=SignatureCheck()
+
+# SignatureCheck.py ends here
