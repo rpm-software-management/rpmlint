f8f23fd95b3b41d183da2739bac6c46d86cc70ba
Frédéric Lepied <flepied@mandriva.com>
2000-10-12 05:07:24 +0000

    cleanup.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@321 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- SourceCheck.py
+++ SourceCheck.py
@@ -12,12 +12,13 @@ import AbstractCheck
 import re
 import Config
 
+spec_regex=re.compile(".spec$")
+bz2_regex=re.compile(".bz2$")
+gz_regex=re.compile(".gz$")
+use_bzip2=Config.getOption("UseBzip2", 1)
+
 class SourceCheck(AbstractCheck.AbstractCheck):
 
-    spec_regex=re.compile(".spec$")
-    bz2_regex=re.compile(".bz2$")
-    gz_regex=re.compile(".gz$")
-    use_bzip2=Config.getOption("UseBzip2", 1)
     
     def __init__(self):
 	AbstractCheck.AbstractCheck.__init__(self, "SourceCheck")
@@ -31,30 +32,21 @@ class SourceCheck(AbstractCheck.AbstractCheck):
 	files=pkg.files()
 	spec_file=None
 	for f in files.keys():
-	    if SourceCheck.spec_regex.search(f):
+	    if spec_regex.search(f):
 		if spec_file:
 		    printError(pkg, "multiple-specfiles", spec_file, f)
 		else:
 		    spec_file=f
 	    else:
-		if SourceCheck.use_bzip2:
-		    if not SourceCheck.bz2_regex.search(f):
+		if use_bzip2:
+		    if not bz2_regex.search(f):
 			printWarning(pkg, "source-or-patch-not-bzipped", f)
 		else:
-		    if not SourceCheck.gz_regex.search(f):
+		    if not gz_regex.search(f):
 			printWarning(pkg, "source-or-patch-not-gzipped", f)
 	    perm=files[f][0] & 07777
 	    if perm != 0644:
-		printWarning(pkg, "strange-permission", f, oct(perm))
-	    
-	    # UNCOMMENT THE FOLLOWING TO RETRIEVE THE SPEC FILE CONTENT
-#  	if spec_file:
-#  	    fd=open(pkg.dirName() + "/" + spec_file, "r")
-#  	    if fd:
-#  		spec_content=fd.readlines()
-#  		fd.close()
-#  	    else:
-#  		sys.stderr.write("unabale to open spec file for %s\n" % (pkg.filename,))
+		printWarning(pkg, "strange-permission", f, oct(perm))	    
 		
 check=SourceCheck()
 
