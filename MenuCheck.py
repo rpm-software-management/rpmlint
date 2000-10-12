e61b29ca7e2465b42f99c6bd210cbcdea0a528d8
Frédéric Lepied <flepied@mandriva.com>
2000-10-12 04:59:09 +0000

    check that titles and longtitles begin by a capital letter. check that no version is included in title and longtitle.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@320 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- MenuCheck.py
+++ MenuCheck.py
@@ -18,94 +18,98 @@ import stat
 import Pkg
 
 DEFAULT_VALID_SECTIONS=(
-    "Configuration/Hardware",
-    "Configuration/Packaging",
-    "Configuration/Networking",
-    "Configuration/Printing",
-    "Configuration/Boot and Init",
-    "Configuration/Other",
-    "Applications/Accessibility",
-    "Applications/Development/Interpreters",
-    "Applications/Development/Code generators",
-    "Applications/Development/Development environments",
-    "Applications/Development/Tools",
-    "Applications/Sciences/Astronomy",
-    "Applications/Sciences/Biology",
-    "Applications/Sciences/Chemistry",
-    "Applications/Sciences/Computer science",
-    "Applications/Sciences/Geosciences",
-    "Applications/Sciences/Mathematics",
-    "Applications/Sciences/Physics",
-    "Applications/Sciences/Other",
-    "Applications/Communications",
-    "Applications/Editors",
-    "Applications/Emulators",
-    "Applications/Archiving/Compression",
-    "Applications/Archiving/Cd burning",
-    "Applications/Archiving/Backup",
-    "Applications/Archiving/Other",
-    "Applications/Monitoring",
-    "Applications/Publishing",
-    "Terminals",
-    "Applications/Shells",
-    "Applications/File tools",
-    "Applications/Text tools",
-    "Documentation",
-    "Office",
-    "Networking/File transfer",
-    "Networking/IRC",
-    "Networking/Instant messaging",
-    "Networking/Chat",
-    "Networking/News",
-    "Networking/Mail",
-    "Networking/WWW",
-    "Networking/Remote access",
-    "Networking/Other",
-    "Amusement/Adventure",
-    "Amusement/Arcade",
-    "Amusement/Boards",
-    "Amusement/Cards",
-    "Amusement/Puzzles",
-    "Amusement/Sports",
-    "Amusement/Strategy",
-    "Amusement/Other",
-    "Amusement/Toys",
-    "Multimedia/Sound",
-    "Multimedia/Graphics",
-    "Multimedia/Video",
-    "Session/Windowmanagers"
+    'Configuration/Hardware',
+    'Configuration/Packaging',
+    'Configuration/Networking',
+    'Configuration/Printing',
+    'Configuration/Boot and Init',
+    'Configuration/Other',
+    'Applications/Accessibility',
+    'Applications/Development/Interpreters',
+    'Applications/Development/Code generators',
+    'Applications/Development/Development environments',
+    'Applications/Development/Tools',
+    'Applications/Sciences/Astronomy',
+    'Applications/Sciences/Biology',
+    'Applications/Sciences/Chemistry',
+    'Applications/Sciences/Computer science',
+    'Applications/Sciences/Geosciences',
+    'Applications/Sciences/Mathematics',
+    'Applications/Sciences/Physics',
+    'Applications/Sciences/Other',
+    'Applications/Communications',
+    'Applications/Editors',
+    'Applications/Emulators',
+    'Applications/Archiving/Compression',
+    'Applications/Archiving/Cd burning',
+    'Applications/Archiving/Backup',
+    'Applications/Archiving/Other',
+    'Applications/Monitoring',
+    'Applications/Publishing',
+    'Terminals',
+    'Applications/Shells',
+    'Applications/File tools',
+    'Applications/Text tools',
+    'Documentation',
+    'Office',
+    'Networking/File transfer',
+    'Networking/IRC',
+    'Networking/Instant messaging',
+    'Networking/Chat',
+    'Networking/News',
+    'Networking/Mail',
+    'Networking/WWW',
+    'Networking/Remote access',
+    'Networking/Other',
+    'Amusement/Adventure',
+    'Amusement/Arcade',
+    'Amusement/Boards',
+    'Amusement/Cards',
+    'Amusement/Puzzles',
+    'Amusement/Sports',
+    'Amusement/Strategy',
+    'Amusement/Other',
+    'Amusement/Toys',
+    'Multimedia/Sound',
+    'Multimedia/Graphics',
+    'Multimedia/Video',
+    'Session/Windowmanagers'
     )
 
 DEFAULT_EXTRA_MENU_NEEDS = (
-    "gnome",
-    "icewm",
-    "kde",
-    "wmaker"
+    'gnome',
+    'icewm',
+    'kde',
+    'wmaker'
     )
 
-DEFAULT_ICON_PATH = (("/usr/share/icons/", "normal"),
-                     ("/usr/share/icons/mini/", "mini"),
-                     ("/usr/share/icons/large/", "large"))
+DEFAULT_ICON_PATH = (('/usr/share/icons/', 'normal'),
+                     ('/usr/share/icons/mini/', 'mini'),
+                     ('/usr/share/icons/large/', 'large'))
+
+menu_file_regex=re.compile('^/usr/lib/menu/([^/]+)$')
+old_menu_file_regex=re.compile('^/usr/share/(gnome/apps|applnk)/([^/]+)$')
+package_regex=re.compile('\?package\((.*)\):')
+needs_regex=re.compile('needs=(\"([^\"]+)\"|([^ \t\"]+))')
+section_regex=re.compile('section=(\"([^\"]+)\"|([^ \t\"]+))')
+title_regex=re.compile('[\"\s]title=(\"([^\"]+)\"|([^ \t\"]+))')
+longtitle_regex=re.compile('longtitle=(\"([^\"]+)\"|([^ \t\"]+))')
+command_regex=re.compile('command=\"?([^\" ]+)')
+kdesu_command_regex=re.compile('[/usr/bin/]?kdesu -c \"?([^ \t\"]+)\"?')
+kdesu_bin_regex=re.compile('[/usr/bin/]?kdesu')
+icon_regex=re.compile('icon=\"?([^\" ]+)')
+valid_sections=Config.getOption('ValidMenuSections', DEFAULT_VALID_SECTIONS)
+update_menus_regex=re.compile('^[^#]*update-menus',re.MULTILINE)
+standard_needs=Config.getOption('ExtraMenuNeeds', DEFAULT_EXTRA_MENU_NEEDS)
+icon_paths=Config.getOption('IconPath', DEFAULT_ICON_PATH)
+xpm_ext_regex=re.compile('/usr/share/icons/(mini/|large/).*\.xpm$')
+capital_regex=re.compile('[0-9A-Z]')
+version_regex=re.compile('([0-9.][0-9.]+)($|\s)')
 
 class MenuCheck(AbstractCheck.AbstractCheck):
-    menu_file=re.compile("^/usr/lib/menu/([^/]+)$")
-    old_menu_file=re.compile("^/usr/share/(gnome/apps|applnk)/([^/]+)$")
-    package=re.compile("\?package\((.*)\):")
-    needs=re.compile("needs=(\"([^\"]+)\"|([^ \t\"]+))")
-    section=re.compile("section=(\"([^\"]+)\"|([^ \t\"]+))")
-    title=re.compile("title=(\"([^\"]+)\"|([^ \t\"]+))")
-    command=re.compile("command=\"?([^\" ]+)")
-    kdesu_command=re.compile("[/usr/bin/]?kdesu -c \"?([^ \t\"]+)\"?")
-    kdesu_bin=re.compile("[/usr/bin/]?kdesu")
-    icon=re.compile("icon=\"?([^\" ]+)")
-    valid_sections=Config.getOption("ValidMenuSections", DEFAULT_VALID_SECTIONS)
-    update_menus=re.compile("^[^#]*update-menus",re.MULTILINE)
-    standard_needs=Config.getOption("ExtraMenuNeeds", DEFAULT_EXTRA_MENU_NEEDS)
-    icon_paths=Config.getOption("IconPath", DEFAULT_ICON_PATH)
-    xpm_ext=re.compile("/usr/share/icons/(mini/|large/).*\.xpm$")
     
     def __init__(self):
-        AbstractCheck.AbstractCheck.__init__(self, "MenuCheck")
+        AbstractCheck.AbstractCheck.__init__(self, 'MenuCheck')
 
     def check(self, pkg, verbose):
 	# Check only binary package
@@ -119,130 +123,148 @@ class MenuCheck(AbstractCheck.AbstractCheck):
         
         for f in files.keys():
             # Check menu files
-            res=MenuCheck.menu_file.search(f)
+            res=menu_file_regex.search(f)
             if res:
                 basename=res.group(1)
                 mode=files[f][0]
                 if not stat.S_ISREG(mode):
-                    printError(pkg, "non-file-in-menu-dir", f)
+                    printError(pkg, 'non-file-in-menu-dir', f)
                 else:
                     if basename != pkgname:
-                        printWarning(pkg, "non-coherent-menu-filename", f)
+                        printWarning(pkg, 'non-coherent-menu-filename', f)
                     if mode & 0444 != 0444:
-                        printError(pkg, "non-readable-menu-file", f)
+                        printError(pkg, 'non-readable-menu-file', f)
                     menus.append(f)
             else:
                 # Check old menus from KDE and GNOME
-                res=MenuCheck.old_menu_file.search(f)
+                res=old_menu_file_regex.search(f)
                 if res:
                     mode=files[f][0]
                     if stat.S_ISREG(mode):
-                        printError(pkg, "old-menu-entry", f)
+                        printError(pkg, 'old-menu-entry', f)
                 else:
                     # Check non transparent xpm files
-                    res=MenuCheck.xpm_ext.search(f)
+                    res=xpm_ext_regex.search(f)
                     if res:
                         mode=files[f][0]
-                        if stat.S_ISREG(mode) and not Pkg.grep("None\",", dirname + "/" + f):
-                            printWarning(pkg, "non-transparent-xpm", f)
+                        if stat.S_ISREG(mode) and not Pkg.grep('None\',', dirname + '/' + f):
+                            printWarning(pkg, 'non-transparent-xpm', f)
         if len(menus) > 0:
             dir=pkg.dirName()
             if menus != []:
                 postin=pkg[rpm.RPMTAG_POSTIN] or pkg[rpm.RPMTAG_POSTINPROG]
                 if not postin:
-                    printError(pkg, "menu-without-postin")
+                    printError(pkg, 'menu-without-postin')
                 else:
-                    if not MenuCheck.update_menus.search(postin):
-                        printError(pkg, "postin-without-update-menus")                    
+                    if not update_menus_regex.search(postin):
+                        printError(pkg, 'postin-without-update-menus')                    
                     
                 postun=pkg[rpm.RPMTAG_POSTUN] or pkg[rpm.RPMTAG_POSTUNPROG]
                 if not postun:
-                    printError(pkg, "menu-without-postun")
+                    printError(pkg, 'menu-without-postun')
                 else:
-                    if not MenuCheck.update_menus.search(postun):
-                        printError(pkg, "postun-without-update-menus")
+                    if not update_menus_regex.search(postun):
+                        printError(pkg, 'postun-without-update-menus')
 
             for f in menus:
                 # remove comments and handle cpp continuation lines
-                str="/lib/cpp %s%s 2>/dev/null| grep ^\?" % (dir, f)
+                str='/lib/cpp %s%s 2>/dev/null| grep ^\?' % (dir, f)
                 cmd=commands.getoutput(str)
-                for line in string.split(cmd, "\n"):
-                    res=MenuCheck.package.search(line)
+                for line in string.split(cmd, '\n'):
+                    res=package_regex.search(line)
                     if res:
                         package=res.group(1)
                         if package != pkgname:
-                            printWarning(pkg, "incoherent-package-value-in-menu", package, f)
+                            printWarning(pkg, 'incoherent-package-value-in-menu', package, f)
                     else:
-                        printInfo(pkg, "unable-to-parse-menu-entry", line)
+                        printInfo(pkg, 'unable-to-parse-menu-entry', line)
 
                     command=1
-                    res=MenuCheck.command.search(line)
+                    res=command_regex.search(line)
                     if res:
                         command=res.group(1)
                         try:
-                            if MenuCheck.kdesu_bin.search(command):
+                            if kdesu_bin_regex.search(command):
                                 found=0
                                 for i in pkg.requires() + pkg.prereq():
-                                    if i[0] == "kdesu" or i[0] == "/usr/bin/kdesu" : found=1
-                                if not found: printError(pkg, "use-of-kdesu-in-menu-but-not-in-requires")
+                                    if i[0] == 'kdesu' or i[0] == '/usr/bin/kdesu' : found=1
+                                if not found: printError(pkg, 'use-of-kdesu-in-menu-but-not-in-requires')
 
-                                res2=MenuCheck.kdesu_command.search(line)
+                                res2=kdesu_command_regex.search(line)
                                 if res2:
                                     command=res2.group(1)
 
                             if command[0] == '/':
                                 files[command]
                             else:
-                                if not (files.has_key("/bin/" + command) or
-                                        files.has_key("/usr/bin/" + command) or
-                                        files.has_key("/usr/X11R6/bin/" + command)):
+                                if not (files.has_key('/bin/' + command) or
+                                        files.has_key('/usr/bin/' + command) or
+                                        files.has_key('/usr/X11R6/bin/' + command)):
                                     raise KeyError, command
                         except KeyError:
-                            printWarning(pkg, "menu-command-not-in-package", command)
+                            printWarning(pkg, 'menu-command-not-in-package', command)
                     else:
                         command=0
 
-                    res=MenuCheck.title.search(line)
+                    res=title_regex.search(line)
                     if res:
                         grp=res.groups()
                         title=grp[1] or grp[2]
+                        if not capital_regex.search(title[0]):
+                            printWarning(pkg, 'menu-title-not-capitalized', title)
+                        res=version_regex.search(title)
+                        if res:
+                            printWarning(pkg, 'version-in-menu-title', title)
                     else:
-                        printError(pkg, "no-title-in-menu", f)
+                        printError(pkg, 'no-title-in-menu', f)
                         title=None
                         
-                    res=MenuCheck.needs.search(line)
+                    res=longtitle_regex.search(line)
+                    if res:
+                        grp=res.groups()
+                        title=grp[1] or grp[2]
+                        if not capital_regex.search(title[0]):
+                            printWarning(pkg, 'menu-longtitle-not-capitalized', title)
+                        res=version_regex.search(title)
+                        if res:
+                            printWarning(pkg, 'version-in-menu-longtitle', title)
+                    else:
+                        printError(pkg, 'no-longtitle-in-menu', f)
+                        title=None
+                        
+                    res=needs_regex.search(line)
                     if res:
                         grp=res.groups()
                         needs=string.lower(grp[1] or grp[2])
-                        if needs in ("x11", "text" ,"wm"):
-                            res=MenuCheck.section.search(line)
+                        if needs in ('x11', 'text' ,'wm'):
+                            res=section_regex.search(line)
                             if res:
                                 grp=res.groups()
                                 section=grp[1] or grp[2]
                                 # don't warn entries for sections
                                 if command:
-                                    if section not in MenuCheck.valid_sections:
-                                        printError(pkg, "invalid-menu-section", section, f)
+                                    if section not in valid_sections:
+                                        printError(pkg, 'invalid-menu-section', section, f)
                             else:
-                                printInfo(pkg, "unable-to-parse-menu-section", line)
-                        elif needs not in MenuCheck.standard_needs:
-                            printInfo(pkg, "strange-needs", needs, f)
+                                printInfo(pkg, 'unable-to-parse-menu-section', line)
+                        elif needs not in standard_needs:
+                            printInfo(pkg, 'strange-needs', needs, f)
                     else:
-                        printInfo(pkg, "unable-to-parse-menu-needs", line)
+                        printInfo(pkg, 'unable-to-parse-menu-needs', line)
 
-                    res=MenuCheck.icon.search(line)
+                    res=icon_regex.search(line)
                     if res:
                         icon=res.group(1)
                         if icon[0] == '/':
-                            printWarning(pkg, "hardcoded-path-in-menu-icon", icon)
+                            printWarning(pkg, 'hardcoded-path-in-menu-icon', icon)
                         else:
-                            for path in MenuCheck.icon_paths:
+                            for path in icon_paths:
                                 try:
                                     files[path[0] + icon]
                                 except KeyError:
-                                    printError(pkg, path[1] + "-icon-not-in-package", icon, f)
+                                    printError(pkg, path[1] + '-icon-not-in-package', icon, f)
                     else:
-                        printWarning(pkg, "no-icon-in-menu", title)
+                        printWarning(pkg, 'no-icon-in-menu', title)
                         
 # Create an object to enable the auto registration of the test
 check=MenuCheck()
