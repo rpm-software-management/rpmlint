a3f12fbefbbb5053106075061d3c4c231a7bc52f
Frédéric Lepied <flepied@mandriva.com>
2000-11-23 15:31:39 +0000

    check if a package provides sources.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@352 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- FilesCheck.py
+++ FilesCheck.py
@@ -120,35 +120,36 @@ STANDARD_DIRS=(
 
 DEFAULT_GAMES_GROUPS='Games'
 
-tmp_regex=re.compile("^/tmp/|^(/var|/usr)/tmp/")
-mnt_regex=re.compile("^/mnt/")
-opt_regex=re.compile("^/opt/")
-home_regex=re.compile("^/home/")
-etc_regex=re.compile("^/etc/")
-sub_bin_regex=re.compile("^(/usr)?/s?bin/\S+/")
-backup_regex=re.compile("~$|\#[^/]+\#$")
-compr_regex=re.compile("\.(gz|z|Z|zip|bz2)$")
-absolute_regex=re.compile("^/([^/]+)")
-absolute2_regex=re.compile("^/?([^/]+)")
-points_regex=re.compile("^../(.*)")
-doc_regex=re.compile("^/usr/(doc|man|info)|^/usr/share/(doc|man|info)")
-bin_regex=re.compile("^(/usr)?/s?bin/")
-includefile_regex=re.compile("\.h$|\.a$")
-sofile_regex=re.compile("\.so$")
-devel_regex=re.compile("-(devel|source)$")
-lib_regex=re.compile("lib/lib[^/]*\.so\..*")
-ldconfig_regex=re.compile("^[^#]*ldconfig", re.MULTILINE)
-info_regex=re.compile("^/usr/share/info")
-install_info_regex=re.compile("^[^#]*install-info", re.MULTILINE)
-perl_temp_file=re.compile(".*perl.*(bs|\.packlist)$")
-cvs_regex=re.compile("/CVS/[^/]+$")
+tmp_regex=re.compile('^/tmp/|^(/var|/usr)/tmp/')
+mnt_regex=re.compile('^/mnt/')
+opt_regex=re.compile('^/opt/')
+home_regex=re.compile('^/home/')
+etc_regex=re.compile('^/etc/')
+sub_bin_regex=re.compile('^(/usr)?/s?bin/\S+/')
+backup_regex=re.compile('~$|\#[^/]+\#$')
+compr_regex=re.compile('\.(gz|z|Z|zip|bz2)$')
+absolute_regex=re.compile('^/([^/]+)')
+absolute2_regex=re.compile('^/?([^/]+)')
+points_regex=re.compile('^../(.*)')
+doc_regex=re.compile('^/usr/(doc|man|info)|^/usr/share/(doc|man|info)')
+bin_regex=re.compile('^(/usr)?/s?bin/')
+includefile_regex=re.compile('\.h$|\.a$')
+sofile_regex=re.compile('/lib/[^/]+\.so$')
+devel_regex=re.compile('-(devel|source)$')
+lib_regex=re.compile('lib/lib[^/]*\.so\..*')
+ldconfig_regex=re.compile('^[^#]*ldconfig', re.MULTILINE)
+info_regex=re.compile('^/usr/share/info')
+install_info_regex=re.compile('^[^#]*install-info', re.MULTILINE)
+perl_temp_file=re.compile('.*perl.*(bs|\.packlist)$')
+cvs_regex=re.compile('/CVS/[^/]+$')
 games_path_regex=re.compile('/usr/(lib/)?/games')
 games_group_regex=re.compile(Config.getOption('RpmGamesGroups', DEFAULT_GAMES_GROUPS))
+source_regex=re.compile('(.c|.cc|.cpp|.ui)$')
 
 class FilesCheck(AbstractCheck.AbstractCheck):
 
     def __init__(self):
-	AbstractCheck.AbstractCheck.__init__(self, "FilesCheck")
+	AbstractCheck.AbstractCheck.__init__(self, 'FilesCheck')
 
     def check(self, pkg, verbose):
 	# Check only binary package
@@ -162,9 +163,9 @@ class FilesCheck(AbstractCheck.AbstractCheck):
 	config_files=pkg.configFiles()
 	ghost_files=pkg.ghostFiles()
 	doc_files=pkg.docFiles()
-
+        
 	if doc_files == []:
-	    printWarning(pkg, "no-documentation")
+	    printWarning(pkg, 'no-documentation')
 	    
 	for f in files.keys():
 	    enreg=files[f]
@@ -173,47 +174,47 @@ class FilesCheck(AbstractCheck.AbstractCheck):
 	    group=enreg[2]
 
 	    if stat.S_ISREG(mode) and doc_regex.search(f) and not f in doc_files:
-		printError(pkg, "not-listed-as-documentation", f)
+		printError(pkg, 'not-listed-as-documentation', f)
 
 	    if not user in STANDARD_USERS:
-		printError(pkg, "non-standard-uid", f, user)
+		printError(pkg, 'non-standard-uid', f, user)
 	    if not group in STANDARD_GROUPS:
-		printError(pkg, "non-standard-gid", f, group)
+		printError(pkg, 'non-standard-gid', f, group)
 
             if stat.S_ISREG(mode):
                 # check ldconfig call in %post and %postun
                 if lib_regex.search(f):
                     postin=pkg[rpm.RPMTAG_POSTIN] or pkg[rpm.RPMTAG_POSTINPROG]
                     if not postin:
-                        printError(pkg, "library-without-ldconfig-postin", f)
+                        printError(pkg, 'library-without-ldconfig-postin', f)
                     else:
                         if not ldconfig_regex.search(postin):
-                            printError(pkg, "postin-without-ldconfig", f)                    
+                            printError(pkg, 'postin-without-ldconfig', f)                    
                         
                     postun=pkg[rpm.RPMTAG_POSTUN] or pkg[rpm.RPMTAG_POSTUNPROG]
                     if not postun:
-                        printError(pkg, "library-without-ldconfig-postun", f)
+                        printError(pkg, 'library-without-ldconfig-postun', f)
                     else:
                         if not ldconfig_regex.search(postun):
-                            printError(pkg, "postun-without-ldconfig", f)
-    
+                            printError(pkg, 'postun-without-ldconfig', f)
+                    
                 # check install-info call in %post and %postun
                 if info_regex.search(f):
                     postin=pkg[rpm.RPMTAG_POSTIN]
                     if not postin:
-                        printError(pkg, "info-files-without-install-info-postin", f)
+                        printError(pkg, 'info-files-without-install-info-postin', f)
                     else:
                         if not install_info_regex.search(postin):
-                            printError(pkg, "postin-without-install-info", f)                    
+                            printError(pkg, 'postin-without-install-info', f)                    
                         
                     postun=pkg[rpm.RPMTAG_POSTUN]
                     preun=pkg[rpm.RPMTAG_PREUN]
                     if not postun and not preun:
-                        printError(pkg, "info-files-without-install-info-postun", f)
+                        printError(pkg, 'info-files-without-install-info-postun', f)
                     else:
                         if (not postun or not install_info_regex.search(postun)) and \
                            (not preun or not install_info_regex.search(preun)):
-                            printError(pkg, "postin-without-install-info", f)
+                            printError(pkg, 'postin-without-install-info', f)
     
                
                 # check perl temp file
@@ -221,30 +222,30 @@ class FilesCheck(AbstractCheck.AbstractCheck):
                     printWarning(pkg, 'perl-temp-file', f)
 
             if tmp_regex.search(f):
-		printError(pkg, "dir-or-file-in-tmp", f)
+		printError(pkg, 'dir-or-file-in-tmp', f)
 	    elif mnt_regex.search(f):
-		printError(pkg, "dir-or-file-in-mnt", f)
+		printError(pkg, 'dir-or-file-in-mnt', f)
 	    elif opt_regex.search(f):
-		printError(pkg, "dir-or-file-in-opt", f)
+		printError(pkg, 'dir-or-file-in-opt', f)
 	    elif sub_bin_regex.search(f):
-		printError(pkg, "subdir-in-bin", f)
+		printError(pkg, 'subdir-in-bin', f)
 	    elif backup_regex.search(f):
-		printError(pkg, "backup-file-in-package", f)
+		printError(pkg, 'backup-file-in-package', f)
             elif home_regex.search(f):
-		printError(pkg, "dir-or-file-in-home", f)
+		printError(pkg, 'dir-or-file-in-home', f)
             elif cvs_regex.search(f):
-		printError(pkg, "cvs-internal-file", f)
-            elif f == "/usr/info/dir" or f == "/usr/share/info/dir":
-                printError(pkg, "info-dir-file", f)
+		printError(pkg, 'cvs-internal-file', f)
+            elif f == '/usr/info/dir' or f == '/usr/share/info/dir':
+                printError(pkg, 'info-dir-file', f)
 	    if etc_regex.search(f) and stat.S_ISREG(mode):
 		if not f in config_files and not f in ghost_files:
-		    printWarning(pkg, "non-conffile-in-etc", f)
+		    printWarning(pkg, 'non-conffile-in-etc', f)
 	    link=enreg[3]
 	    if link != '':
 		ext=compr_regex.search(link)
 		if ext:
-		    if not re.compile("\." + ext.group(1) + "$").search(f):
-			printError(pkg, "compressed-symlink-with-wrong-ext", f, link)
+		    if not re.compile('\.' + ext.group(1) + '$').search(f):
+			printError(pkg, 'compressed-symlink-with-wrong-ext', f, link)
 
 	    perm=mode & 07777
 
@@ -261,60 +262,60 @@ class FilesCheck(AbstractCheck.AbstractCheck):
 		    if stat.S_ISGID & mode:
 			setgid=group
 		    if setuid and setgid:
-			printError(pkg, "setuid-gid-binary", f, setuid, setgid, oct(perm))
+			printError(pkg, 'setuid-gid-binary', f, setuid, setgid, oct(perm))
 		    elif setuid:
-			printError(pkg, "setuid-binary", f, setuid, oct(perm))
+			printError(pkg, 'setuid-binary', f, setuid, oct(perm))
 		    elif setgid:
                         if not (group == 'games' and
                                 (games_path_regex.search(f) or games_group_regex.search(pkg[rpm.RPMTAG_GROUP]))):
-                            printError(pkg, "setgid-binary", f, setgid, oct(perm))
+                            printError(pkg, 'setgid-binary', f, setgid, oct(perm))
 		    elif mode & 0777 != 0755:
-			printError(pkg, "non-standard-executable-perm", f, oct(perm))
+			printError(pkg, 'non-standard-executable-perm', f, oct(perm))
 
             # normal file check
             if stat.S_ISREG(mode):
                 if bin_regex.search(f) and mode & 0111 == 0:
-                    printWarning(pkg, "non-executable-in-bin", f, oct(perm))
+                    printWarning(pkg, 'non-executable-in-bin', f, oct(perm))
                 if not devel_pkg and includefile_regex.search(f):
-                    printWarning(pkg, "devel-file-in-non-devel-package", f)
+                    printWarning(pkg, 'devel-file-in-non-devel-package', f)
                 
 	    # normal executable check
 	    elif stat.S_ISREG(mode) and mode & stat.S_IXUSR:
 		if perm != 0755:
-		    printError(pkg, "non-standard-executable-perm", f, oct(perm))
+		    printError(pkg, 'non-standard-executable-perm', f, oct(perm))
 		    
 	    # normal dir check
             elif stat.S_ISDIR(mode):
                 if perm != 0755:
-                    printError(pkg, "non-standard-dir-perm", f, oct(perm))
+                    printError(pkg, 'non-standard-dir-perm', f, oct(perm))
                 if pkg[rpm.RPMTAG_NAME] != 'filesystem':
                     if f in STANDARD_DIRS:
-                        printError(pkg, "standard-dir-owned-by-package", f)
+                        printError(pkg, 'standard-dir-owned-by-package', f)
 
 	    # symbolic link check
 	    elif stat.S_ISLNK(mode):
 		r=absolute_regex.search(link)
                 is_so=sofile_regex.search(f)
                 if not devel_pkg and is_so:
-                    printWarning(pkg, "devel-file-in-non-devel-package", f)
+                    printWarning(pkg, 'devel-file-in-non-devel-package', f)
 		# absolute link
 		if r:
                     if not is_so and link not in files.keys():
-                        printWarning(pkg, "dangling-symlink", f, link)
+                        printWarning(pkg, 'dangling-symlink', f, link)
 		    linktop=r.group(1)
 		    r=absolute_regex.search(f)
 		    if r:
 			filetop=r.group(1)
 			if filetop == linktop:
 			    # absolute links within one toplevel directory are _not_ ok!
-			    printWarning(pkg ,"symlink-should-be-relative", f, link)
+			    printWarning(pkg ,'symlink-should-be-relative', f, link)
 		# relative link
 		else:
                     if not is_so:
                         file = '%s%s/%s' % (pkg.dirName(), os.path.dirname(f), link)
                         file = os.path.normpath(file)
                         if not os.path.exists(file):
-                            printWarning(pkg, "dangling-symlink", f, link)
+                            printWarning(pkg, 'dangling-symlink', f, link)
 		    pathcomponents=string.split(f, '/')[1:]
 		    r=points_regex.search(link)
 		    lastpop=None
@@ -323,7 +324,7 @@ class FilesCheck(AbstractCheck.AbstractCheck):
 		    while r:
 			mylink=r.group(1)
 			if len(pathcomponents) == 0:
-			    printError(pkg, "symlink-has-too-many-up-segments", f, link)
+			    printError(pkg, 'symlink-has-too-many-up-segments', f, link)
 			    break
 			else:
 			    lastpop=pathcomponents[0]
@@ -336,18 +337,18 @@ class FilesCheck(AbstractCheck.AbstractCheck):
 			
 			# does the link go up and then down into the same directory?
 			#if linktop == lastpop:
-			#    printWarning(pkg, "lengthy-symlink", f, link)
+			#    printWarning(pkg, 'lengthy-symlink', f, link)
 		    
 			if len(pathcomponents) == 0:
 			    # we've reached the root directory
 			    if linktop != lastpop:
 				# relative link into other toplevel directory
-				printWarning(pkg, "symlink-should-be-absolute", f, link)
+				printWarning(pkg, 'symlink-should-be-absolute', f, link)
 			# check additional segments for mistakes like `foo/../bar/'
 			for linksegment in string.split(mylink, '/'):
 			    if linksegment == '..':
-				printError(pkg, "symlink-contains-up-and-down-segments", f, link)
-			    
+				printError(pkg, 'symlink-contains-up-and-down-segments', f, link)
+
 # Create an object to enable the auto registration of the test
 check=FilesCheck()
 
