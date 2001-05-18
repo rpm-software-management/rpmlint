e33eb308968479947f4319af1385a9447030f2da
Frédéric Lepied <flepied@mandriva.com>
2001-05-18 09:17:31 +0000

    factorized checks
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@402 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- check-install.py
+++ check-install.py
@@ -2,7 +2,7 @@
 #---------------------------------------------------------------
 # Project         : Linux-Mandrake
 # Module          : rpmlint
-# File            : check-install
+# File            : check-install.py
 # Version         : $Id$
 # Author          : Frederic Lepied
 # Created On      : Fri Oct 13 20:40:08 2000
@@ -14,10 +14,30 @@ import Pkg
 db=rpm.opendb()
 idx=db.firstkey()
 
+def check_name(db, name):
+    try:
+        return db.findbyname(dep)
+    except:
+        return None
+    
+def check_provides(db, name):
+    if name[0:7] == 'rpmlib(':
+        return 1
+    try:
+        return db.findbyprovides(dep)
+    except:
+        return None
+    
+def check_file(db, name):
+    try:
+        return db.findbyfile(dep)
+    except:
+        return None
+    
 while idx:
     pkg=Pkg.Pkg(None, None, db[idx])
     for (dep,ver,flags) in pkg.requires() + pkg.prereq():
-        if dep != 'rpmlib(VersionedDependencies)' and not db.findbyname(dep) and not db.findbyprovides(dep) and not db.findbyfile(dep):
+        if  not check_name(db, dep) and not check_provides(db, dep) and not check_file(db, dep):
             print 'unsatisfied dependency', dep, 'for', pkg.name
     idx=db.nextkey(idx)
 
