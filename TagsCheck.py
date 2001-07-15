16f4a8392ef458ef9b6fcd72f02c5e1d222c5b66
Frédéric Lepied <flepied@mandriva.com>
2001-07-15 17:05:42 +0000

    added missing descriptions.
    corrected the -devel warning to handle the libbzip2_1-devel case.
    
    
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@482 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- TagsCheck.py
+++ TagsCheck.py
@@ -385,6 +385,7 @@ BAD_WORDS = {
     'xwindows': 'X'
     }
 
+distribution=Config.getOption("Distribution", "Mandrake Linux")
 VALID_GROUPS=Config.getOption('ValidGroups', DEFAULT_VALID_GROUPS)
 VALID_LICENSES=Config.getOption('ValidLicenses', DEFAULT_VALID_LICENSES)
 packager_regex=re.compile(Config.getOption('Packager', DEFAULT_PACKAGER))
@@ -394,7 +395,7 @@ release_ext=Config.getOption('ReleaseExtension', 'mdk')
 extension_regex=release_ext and re.compile(release_ext + '$')
 use_version_in_changelog=Config.getOption('UseVersionInChangelog', 1)
 devel_regex=re.compile('(.*)-devel')
-devel_number_regex=re.compile('(.*?)[0-9.]+-devel')
+devel_number_regex=re.compile('(.*?)([0-9.]+)(_[0-9.])?-devel')
 capital_regex=re.compile('[0-9A-Z]')
 url_regex=re.compile('^(ftp|http|https)://')
 so_regex=re.compile('\.so$')
@@ -477,7 +478,11 @@ class TagsCheck(AbstractCheck.AbstractCheck):
                     if not res:
                         printWarning(pkg, 'no-major-in-name', name)
                     else:
-                        prov=res.group(1) + '-devel'
+                        if res.group(3):
+                            prov=res.group(1) + res.group(2) + '-devel'
+                        else:
+                            prov=res.group(1) + '-devel'
+                            
                         if not prov in map(lambda x: x[0], pkg.provides()):
                             printWarning(pkg, 'no-provides', prov)
                     
@@ -537,7 +542,7 @@ class TagsCheck(AbstractCheck.AbstractCheck):
             printError(pkg, 'no-license')
         else:
             if license not in VALID_LICENSES:
-                licenses=re.split('(?:and|or|&|\s|-like|/|ish|-style|-Style|-)+', license)
+                licenses=re.split('(?:[- ]like|/|ish|[- ]style|[- ]Style|and|or|&|\s|-)+', license)
                 for l in licenses:
                     if l != '' and not l in VALID_LICENSES:
                         printWarning(pkg, 'invalid-license', license)
@@ -560,107 +565,108 @@ if Config.info:
 'invalid-version',
 '''The version string must not contain the pre, alpha, beta or rc suffixes because
 when the final version will be out, you will have to use an Epoch tag to make
-you package upgradable. Instead put it in the release tag like 0.alpha8.1mdk.''',
+you package upgradable. Instead put it in the release tag like 0.alpha8.1''' + release_ext + '.',
 
-'spelling-error-in-',
-"""You made a mispelling in your text. Please double-check.""",
+'spelling-error-in-description',
+'''You made a mispelling in the Description. Please double-check.''',
+
+'spelling-error-in-summary',
+'''You made a mispelling in the Summary. Please double-check.''',
 
 'no-packager-tag',
-"""There is no Packager tag in your package. You have to specify a packager using
-the Package tag. Ex: Packager: Christian Belisle <cbelisle@mandrakesoft.com>""",
+'''There is no Packager tag in your package. You have to specify a packager using
+the Packager tag. Ex: Packager: Christian Belisle <cbelisle@mandrakesoft.com>.''',
 
 'invalid-packager',
-"""The packager email must finish with @mandrakesoft.com or must be bugs@linux-mandrake.com.
-Please change it and rebuild your package.""",
+'''The packager email must finish with @mandrakesoft.com or must be bugs@linux-mandrake.com.
+Please change it and rebuild your package.''',
 
 'no-version-tag',
-"""There is no Version tag in your package. You have to specify a version using the
-Version tag.""",
+'''There is no Version tag in your package. You have to specify a version using the
+Version tag.''',
 
 'no-release-tag',
-"""There is no Release tag in your package. You have to specify a release using the
-Release tag.""",
+'''There is no Release tag in your package. You have to specify a release using the
+Release tag.''',
 
 'not-standard-release-extension',
-"""Your release number must finish with mdk and must be valid.""",
+'Your release number must finish with ' + release_ext + ' and must be valid.',
 
 'no-name-tag',
-"""There is no Name tag in your package. You have to specify a name using the Name tag.""",
+'''There is no Name tag in your package. You have to specify a name using the Name tag.''',
 
 'non-coherent-filename',
-"""
-""",
+'''
+''',
 
 'no-dependency-on',
-"""
-""",
+'''
+''',
 
 'incoherent-version-dependency-on',
-"""
-""",
+'''
+''',
 
 'no-version-dependency-on',
-"""
-""",
+'''
+''',
 
 'no-major-in-name',
-""" 
-""",
+'''The major number of the library isn't contained in the package name.
+''',
 
 'no-provides',
-"""There is no Provides tag in your package. You have to specify at least one
-term designing the application your package is installing. To insert it, just
-insert a tag 'Provide'""",
+'''Your library package doesn't provide the -devel name without the major version
+included.''',
 
 'no-summary-tag',
-"""There is no Summary tag in your package. You have to describe your package
-using this tag. To insert it, just insert a tag 'Summary'""",
+'''There is no Summary tag in your package. You have to describe your package
+using this tag. To insert it, just insert a tag 'Summary'.''',
 
 'summary-on-multiple-lines',
-"""Your summary must fit on one line. Please make it shorter and rebuilt your package.""",
+'''Your summary must fit on one line. Please make it shorter and rebuilt your package.''',
 
 'summary-not-capitalized',
-"""
-""",
+'''Summary doesn't begin with a capital letter.''',
 
 'summary-has-leading-spaces',
-"""
-""",
+'''Summary begins with spaces and that will waste space when displayed.''',
 
 'no-description-tag',
-"""There is no %description tag in your spec file. To insert it, just insert a '%tag' in
-your spec file and rebuild it.""",
+'''There is no %description tag in your spec file. To insert it, just insert a '%tag' in
+your spec file and rebuild it.''',
 
 'description-line-too-long',
-"""Your description lines must no exceed 80 characters. If a line is exceeding this number,
-cut it to fit in two lines.""",
+'''Your description lines must no exceed 80 characters. If a line is exceeding this number,
+cut it to fit in two lines.''',
 
 'no-group-tag',
-"""There is no Group tag in your package. You have to specify a valid group
-in your spec file using the Group tag.""",
+'''There is no Group tag in your package. You have to specify a valid group
+in your spec file using the Group tag.''',
 
 'non-standard-group',
-"""The group specified in your spec file is not valid. To find a valid group,
-please refer to the Mandrake Linux RPM documentation.""",
+'''The group specified in your spec file is not valid. To find a valid group,
+please refer to the ''' + distribution + ' RPM documentation.''',
 
 'no-changelogname-tag',
-"""There is no %changelog tag in your spec file. To insert it, just insert a '%changelog' in
-your spec file and rebuild it.""",
+'''There is no %changelog tag in your spec file. To insert it, just insert a '%changelog' in
+your spec file and rebuild it.''',
 
 'no-version-in-last-changelog',
-"""The last changelog entry doesn't contain a version. Please insert the coherent version and
-rebuild your package.""",
+'''The last changelog entry doesn't contain a version. Please insert the coherent version and
+rebuild your package.''',
 
 'incoherent-version-in-changelog',
-"""Your last entry in %changelog contains a version that is not coherent with the current
-version of your package.""",
+'''Your last entry in %changelog contains a version that is not coherent with the current
+version of your package.''',
 
 'no-license',
-"""There is no License tag in your spec file. You have to specify one license for your
-program (ie GPL). To insert this tag, just insert a 'License' in your file.""",
+'''There is no License tag in your spec file. You have to specify one license for your
+program (ie GPL). To insert this tag, just insert a 'License' in your file.''',
 
 'invalid-license',
-"""The license you specified is invalid. The valid licenses are:
+'''The license you specified is invalid. The valid licenses are:
+
 -GPL					-LGPL
 -Artistic				-BSD
 -MIT					-QPL
@@ -671,10 +677,11 @@ program (ie GPL). To insert this tag, just insert a 'License' in your file.""",
 -Ricoh Source Code Public License	-Python license
 -Vovida Software License		-Sun Internet Standards Source License
 -Intel Open Source License		-Jabber Open Source License
-""",
+
+if the license is near an existing one, you can use '<license> style'.''',
 
 'invalid-url',
-"""Your URL is not valid. It must begin with http, https or ftp.""",
+'''Your URL is not valid. It must begin with http, https or ftp.''',
 
 )
     
