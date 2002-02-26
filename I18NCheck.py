96fd38eabf3b0d3c95bd892a080ea9a6895bd2f8
Pablo Saratxaga <pablo@mandriva.com>
2002-02-26 22:00:56 +0000

    Added 'mt' to recognized locales
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@629 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- I18NCheck.py
+++ I18NCheck.py
@@ -37,8 +37,8 @@ CORRECT_SUBDIRS = (
 'es_PA', 'es_MX', 'et', 'eu', 'fa',
 'fi', 'fo', 'fr', 'ga', 'gd', 'gl', 'gv', 'he', 'hr',
 'hu', 'hy', 'ia', 'id', 'is', 'it', 'ja', 'ka', 'ko',
-'kw', 'lo', 'lt', 'lv', 'ma', 'mk', 'ms', 'nb', 'nl', 'nn', 'no', 
-'oc', 'pd', 'ph', 'pl', 'pp', 'pt', 'pt_BR', 'ro', 'ru', 'sk', 'sl',
+'kw', 'lo', 'lt', 'lv', 'ma', 'mk', 'ms', 'mt', 'nb', 'nl', 'nn', 'no', 
+'oc', 'ph', 'pl', 'pp', 'pt', 'pt_BR', 'ro', 'ru', 'sk', 'sl',
 'sp', 'sq', 'sr', 'sv', 'ta', 'tg', 'th', 'tr', 'tt', 'uk', 'ur', 'uz',
 'vi', 'wa', 'yi', 'zh_CN', 'zh_CN.GB2312', 'zh_TW', 'zh_TW.Big5'
 )
