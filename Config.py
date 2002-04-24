b9d82b2148c201687531c3dac9a9ba2a41dbc8f9
Frédéric Lepied <flepied@mandriva.com>
2002-04-24 20:11:23 +0000

    added load_policy
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@646 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- Config.py
+++ Config.py
@@ -448,4 +448,8 @@ addFilter('shorewall non-standard-dir-perm /etc/shorewall 0700')
 #DansGuardian
 addFilter('DansGuardian non-readable /etc/dansguardian/.* 0600')
 
+def load_policy(policy):
+    if policy != 'mdk9.0':
+        addFilter('no-arch-tag|incoherent-architecture-in-filename')
+    
 # Config.py ends here
