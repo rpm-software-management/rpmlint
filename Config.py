9c9f1ef27290a470ffa6a0adadccdeb8e41f4bc6
Frédéric Lepied <flepied@mandriva.com>
2002-05-29 20:01:46 +0000

    added non-root-user-log-file|non-root-group-log-file|non-ghost-file for mdk9.0 policy.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@661 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- Config.py
+++ Config.py
@@ -450,6 +450,6 @@ addFilter('DansGuardian non-readable /etc/dansguardian/.* 0600')
 
 def load_policy(policy):
     if policy != 'mdk9.0':
-        addFilter('no-prereq-on')
+        addFilter('no-prereq-on|non-root-user-log-file|non-root-group-log-file|non-ghost-file')
         
 # Config.py ends here
