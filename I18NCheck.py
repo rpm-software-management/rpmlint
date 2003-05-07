4347e529aae1122eb43553e98b78eaa629c6c138
Pablo Saratxaga <pablo@mandriva.com>
2003-05-07 14:48:12 +0000

    changed Serbian lang codes to match what is now used in Gnome and KDE
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@746 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- I18NCheck.py
+++ I18NCheck.py
@@ -32,9 +32,6 @@ INCORRECT_LOCALES = {
 # Correct subdirs of /usr/share/local for LC_MESSAGES
 # and /usr/share/man for locale man pages.
 #
-# note: zh_CN.GB2312 and zh_TW.Big5 (that is, names with charset information)
-# are obsolescnet, but still widely used; some day however they should
-# be removed from this list.
 # 'en_RN' and 'en@IPA' are not real language bu funny variations on english
 CORRECT_SUBDIRS = (
 'af', 'am', 'ar', 'az', 'be', 'bg', 'bn', 'br', 'bs', 'ca', 'cs', 'cy',
@@ -45,9 +42,14 @@ CORRECT_SUBDIRS = (
 'ia', 'id', 'is', 'it', 'ja', 'ka', 'kn', 'ko', 'kw', 'lo', 'lt', 'lg', 'lv',
 'mi', 'mk', 'mr', 'ms', 'mt', 'nb', 'nl', 'nn', 'no',
 'oc', 'or', 'ph', 'pl', 'pp', 'pt', 'pt_BR',
-'ro', 'ru', 'sk', 'sl', 'sp', 'sq', 'sr', 'sv',
+'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sr@Latn', 'sv',
 'ta', 'te', 'tg', 'th', 'tr', 'tt', 'uk', 'ur', 'uz', 'vi', 'wa',
-'xh', 'yi', 'zh_CN', 'zh_CN.GB2312', 'zh_HK', 'zh_TW', 'zh_TW.Big5'
+'xh', 'yi', 'zh_CN', 'zh_HK', 'zh_TW', 'zu',
+# 
+# note: zh_CN.GB2312 and zh_TW.Big5 (that is, names with charset information)
+# are obsolescnet, but still widely used; some day however they should
+# be removed from this list.
+'zh_CN.GB2312', 'zh_TW.Big5',
 )
 
 str='-('
