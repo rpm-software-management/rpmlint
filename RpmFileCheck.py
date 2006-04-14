e179337c53767a3a26ab585c15ab48a4c3e78d74
Ville Skyttä <ville.skytta@iki.fi>
2006-04-14 09:07:38 +0000

    Make info message wrap with < 80 char lines.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1152 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- RpmFileCheck.py
+++ RpmFileCheck.py
@@ -22,7 +22,8 @@ check = RpmFileCheck()
 if Config.info:
     addDetails(
 'filename-too-long-for-joliet',
-'This filename is too long to fit on a joliet filesystem ( limit is 64 unicode chars ).',
+'This filename is too long to fit on a joliet filesystem (limit is 64 unicode
+chars).',
 )
 
 # Local variables:
