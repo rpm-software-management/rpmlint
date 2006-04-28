e9eee181349bdfa26f2e8a038ba063fc23757062
Michael Scherer <misc@mandriva.org>
2006-04-28 12:16:21 +0000

    - create a new class AbstractFilesCheck to factorise the code that filter and extract files based on a regexp. this class is now used for PamCheck.py, and other similar check will be converted soon.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1158 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- AbstractCheck.py
+++ AbstractCheck.py
@@ -17,6 +17,29 @@ class AbstractCheck:
     def check(self, pkg):
         raise NotImplementedError('check must be implemented in subclass')
 
+
+import re
+
+class AbstractFilesCheck(AbstractCheck):
+    def __init__(self, name, file_regexp):
+        self.__files_re = re.compile(file_regexp)
+        AbstractCheck.__init__(self, name)
+    def check(self, pkg):
+        if pkg.isSource():
+            return
+        dirname = pkg.dirName()
+        files = []
+        for f in pkg.files().keys():
+            if self.__files_re.match(f):
+                files.append(f)
+        for i in files:
+            self.check_file(pkg, i)
+
+ 
+    def check_file(self, pkg, file):
+        """ Virtual method called for each file that match the regexp passed to the constructor """
+        raise NotImplementedError('check must be implemented in subclass')
+        
 # AbstractCheck.py ends here
 
 # Local variables:
--- PamCheck.py
+++ PamCheck.py
@@ -13,31 +13,19 @@ import AbstractCheck
 import rpm
 import re,os,time,subprocess
 import Config
-pam_re=re.compile('/etc/pam\.d/.*')
 pam_stack_re=re.compile('pam_stack.so\s*service')
 
-class PamCheck(AbstractCheck.AbstractCheck):
-
+class PamCheck(AbstractCheck.AbstractFilesCheck):
     def __init__(self):
-        AbstractCheck.AbstractCheck.__init__(self, "PamCheck")
-
-    def check(self, pkg):
-        if pkg.isSource():
-            return
-        dirname=pkg.dirName()
-        pam_files=[]
-        for f in pkg.files().keys():
-            if pam_re.match(f):
-                pam_files.append(f)
-        for i in pam_files:
-            self.check_pam_file(pkg, dirname, i)
-
-    def check_pam_file(self, pkg, directory, filename):
-        f = open(directory + filename)
+        AbstractCheck.AbstractFilesCheck.__init__(self, "PamCheck", "/etc/pam\.d/.*")
+    
+    def check_file(self, pkg, filename):
+        f = open(pkg.dirName() + filename)
         for l in f.readlines():
             if pam_stack_re.search(l):
                 printError(pkg, 'use-old-pam-stack',filename)
         f.close()
+
 check=PamCheck()
 
 if Config.info:
