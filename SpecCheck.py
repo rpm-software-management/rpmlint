79ddf8ac4529f2eedcc6902faf10099cbce00f71
Ville Skyttä <ville.skytta@iki.fi>
2006-06-05 18:45:55 +0000

    Tighten section marker regexp.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1184 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- SpecCheck.py
+++ SpecCheck.py
@@ -39,7 +39,7 @@ setup_regex = re.compile('^%setup')
 section = {}
 for sec in ['description', 'prep', 'build', 'install', 'clean', 'files', 'changelog', 'package', 'check']:
 	section[sec] = {}
-	section[sec]['re'] = re.compile('^%' + sec)
+	section[sec]['re'] = re.compile('^%' + sec + '(?:\s|$)')
 rpm_buildroot_regex = re.compile('\${?RPM_BUILD_ROOT}?|%{?buildroot}?')
 configure_start_regex = re.compile('\./configure')
 configure_libdir_spec_regex = re.compile('ln |\./configure[^#]*--libdir=([^\s]+)[^#]*')
