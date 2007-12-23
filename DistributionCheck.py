9a41db34b8ed4d7bb97ecf1c329232e36bbb46ca
Michael Scherer <misc@mandriva.org>
2007-12-23 10:49:15 +0000

    - check manpages compressed with lzma, asked by lzma lover dvalin :) ( and blino@mandriva )
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1391 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- DistributionCheck.py
+++ DistributionCheck.py
@@ -19,6 +19,7 @@ info_dir_regex=re.compile("/info/dir$")
 vendor=Config.getOption("Vendor")
 distribution=Config.getOption("Distribution")
 use_bzip2=Config.getOption("UseBzip2", 1)
+use_lzma=Config.getOption("UseLzma", 0)
 
 class DistributionCheck(AbstractCheck.AbstractCheck):
 
@@ -42,12 +43,19 @@ class DistributionCheck(AbstractCheck.AbstractCheck):
                 if use_bzip2:
                     if not f.endswith('.bz2'):
                         printWarning(pkg, "manpage-not-bzipped", f)
+                if use_lzma:
+                    if not f.endswith('.lzma'):
+                        printWarning(pkg, "manpage-not-compressed-wth-lzma", f)
+     
                 elif not f.endswith('.gz'):
                     printWarning(pkg, "manpage-not-gzipped", f)
             if info_regex.search(f) and not info_dir_regex.search(f):
                 if use_bzip2:
                     if not f.endswith('.bz2'):
                         printWarning(pkg, "infopage-not-bzipped", f)
+                if use_lzma:
+                    if not f.endswith('.lzma'):
+                        printWarning(pkg, "infopage-not-compressed-wth-lzma", f)
                 elif not f.endswith('.gz'):
                     printWarning(pkg, "infopage-not-gzipped", f)
 
@@ -62,6 +70,12 @@ if Config.info:
 'invalid-distribution',
 'The distribution value should be "' + distribution + '".',
 
+'manpage-not-compressed-with-lzma',
+'''Manual Pages are not compressed using the .lzma extension/format. Please
+run lzma <man page file> to compress it in the %install section and
+rebuild the package. You can also use the spec-helper package
+that automates this task.''',
+
 'manpage-not-bzipped',
 '''Manual Pages are not compressed using the .bz2 extension/format. Please
 run bzip2 <man page file> to bzip it in the %install section and
@@ -72,6 +86,12 @@ that automates this task.''',
 '''Manual Pages are not compressed using the .gz extension/format. Please
 run gzip <man page file> to gzip it in the %install section and rebuild the
 package.''',
+
+'infopage-not-compressed-with-lzma',
+'''An info page are not compressed using the .lzma extension/format. Please
+run lzma <info page file> to compress it in the %install section and
+rebuild the package. You can also use the spec-helper package
+that automates this task.''',
 
 'infopage-not-bzipped',
 '''An info page is not compressed using the .bz2 extension/format. Please
