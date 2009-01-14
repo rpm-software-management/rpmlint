352186ab65c864971e8e730376209dadd8a43ec9
Ville Skyttä <ville.skytta@iki.fi>
2009-01-14 17:49:22 +0000

    Improve applied patch detection (#59, dmueller at suse.de), add test case.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1484 9bc8b190-ac0f-0410-8968-dc7d1f502856

---
+++ test/test.SpecCheck.py
@@ -0,0 +1,18 @@
+import re
+import unittest
+import Testing
+import SpecCheck
+
+class TestSpecCheck(unittest.TestCase):
+    def setUp(self):
+        self.pkg = Testing.getTestedSpecPackage('SpecCheck')
+        Testing.startTest()
+    def testcheck(self):
+        SpecCheck.check.check_spec(self.pkg, self.pkg.name)
+        out = "\n".join(Testing.getOutput())
+        self.assertTrue(re.search("patch-not-applied Patch3", out));
+        self.assertFalse(re.search("patch-not-applied Patch\\b", out));
+        self.assertFalse(re.search("patch-not-applied Patch[0124]", out));
+
+if __name__ == '__main__':
+    unittest.main()
