rpm-software-management/rpmlint

commit f8541c800425f3a9bc2a5d851764802eb3a6deaf

- do not expand the format of the filename format, (patch from Per Oyvind Karlsten )
git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1729 9bc8b190-ac0f-0410-8968-dc7d1f502856

TagsCheck.py
@@ -784,7 +784,7 @@ class TagsCheck(AbstractCheck.AbstractCheck):
                     j += 1
                 i += 1
 
-        expected = '%s-%s-%s.%s.rpm' % (name, version, release, pkg.arch)
+        expected = pkg.header.sprintf(rpm.expandMacro("%{_build_name_fmt}")).split("/")[-1]
         basename = os.path.basename(pkg.filename)
         if basename != expected:
             printWarning(pkg, 'non-coherent-filename', basename, expected)

commit 545314b26f1ba2d4d7f20e786b2781fabd4633a7

Fix non-coherent-filename for source packages (regression in r1729).
git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1738 9bc8b190-ac0f-0410-8968-dc7d1f502856

TagsCheck.py
@@ -803,8 +803,12 @@ class TagsCheck(AbstractCheck.AbstractCheck):
                     j += 1
                 i += 1
 
-        expected = pkg.header.sprintf(
-            rpm.expandMacro("%{_build_name_fmt}")).split("/")[-1]
+        expfmt = rpm.expandMacro("%{_build_name_fmt}")
+        if pkg.isSource():
+            # _build_name_fmt often (always?) ends up not outputting src/nosrc
+            # as arch for source packages, do it ourselves
+            expfmt = re.sub(r'(?i)%\{?ARCH\b\}?', pkg.arch, expfmt)
+        expected = pkg.header.sprintf(expfmt).split("/")[-1]
         basename = os.path.basename(pkg.filename)
         if basename != expected:
             printWarning(pkg, 'non-coherent-filename', basename, expected)