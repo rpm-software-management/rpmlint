8c149605a5f6d0371e43369b7769faf3e3975c1f
Ville Skyttä <ville.skytta@iki.fi>
2010-04-26 16:08:37 +0000

    Clean up after ourselves.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1777 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- test/test.PamCheck.py
+++ test/test.PamCheck.py
@@ -9,6 +9,8 @@ class TestPamCheck(unittest.TestCase):
     def setUp(self):
         self.pkg = Testing.getTestedPackage('PamCheck')
         Testing.startTest()
+    def tearDown(self):
+        self.pkg.cleanup()
     def testcheck(self):
         PamCheck.check.check(self.pkg)
         self.assertEqual( Testing.getOutput(), ['PamCheck.i586: E: use-old-pam-stack /etc/pam.d/PamCheck (line 1)'])
