2937b241e0631f43675d722158272d4fd41cf8ec
Ville Skyttä <ville.skytta@iki.fi>
2010-06-05 19:55:53 +0000

    Fix self-obsoletion bug with Provides containing Epoch.
    git-svn-id: svn+ssh://rpmlint.zarb.org/home/projects/rpmlint/svn/trunk@1794 9bc8b190-ac0f-0410-8968-dc7d1f502856

--- test/test.Pkg.py
+++ test/test.Pkg.py
@@ -9,6 +9,7 @@ import Pkg
 class TestPkg(unittest.TestCase):
     def setUp(self):
         Testing.startTest()
+
     def test_parse_deps(self):
         for (arg, exp) in (
             ("a, b < 1.0 c = 5:2.0-3 d",
@@ -18,6 +19,13 @@ class TestPkg(unittest.TestCase):
               ("d", 0, (None, None, None))]),
             ):
             self.assertEqual(Pkg.parse_deps(arg), exp)
+
+    def test_rangeCompare(self):
+        for (req, prov) in (
+            (("foo", rpm.RPMSENSE_LESS, (None, "1.0", None)),
+             ("foo", rpm.RPMSENSE_EQUAL, ("1", "0.5", None))),
+            ):
+            self.assertFalse(Pkg.rangeCompare(req, prov))
 
 if __name__ == '__main__':
     unittest.main()
