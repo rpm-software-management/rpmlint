29a257991e4078c3e7c7cf3dd1667308fcf5e95e
Michael Scherer <misc@zarb.org>
2013-11-01 22:58:09 +0100

    Add a check that validate appdata file

---
+++ AppDataCheck.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#
+# check appdata file format violation
+#
+# http://people.freedesktop.org/~hughsient/appdata/
+#
+
+from Filter import addDetails, printError, printWarning
+from Pkg import getstatusoutput
+import AbstractCheck
+
+STANDARD_BIN_DIRS = ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/']
+
+
+class AppDataCheck(AbstractCheck.AbstractFilesCheck):
+    def __init__(self):
+        # desktop file need to be in $XDG_DATA_DIRS
+        # $ echo $XDG_DATA_DIRS/applications
+        # /var/lib/menu-xdg:/usr/share
+        AbstractCheck.AbstractFilesCheck.__init__(
+            self, "AppDataCheck", "/usr/share/appdata/.*\.appdata.xml$")
+
+    def check_file(self, pkg, filename):
+        root = pkg.dirName()
+        f = root + filename
+        st = getstatusoutput(('appdata-validate', f), True)
+        if st[0]:
+            error_printed = False
+            for line in st[1].splitlines():
+                if 'problems detected' in line:
+                    printError(pkg, 'invalid-appdate-file', filename)
+
+
+check = AppDataCheck()
+
+addDetails(
+'invalid-appdata-file',
+'''appdata file is not valid, check with appdata-validate''',
+)
+
+# Local variables:
+# indent-tabs-mode: nil
+# py-indent-offset: 4
+# End:
+# ex: ts=4 sw=4 et
