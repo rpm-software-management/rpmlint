rpm-software-management/rpmlint

commit fff597bbfba116c739d5cf15c60176ad99ddff66

FilesCheck: Look for shebang only at start of file

FilesCheck.py
@@ -220,7 +220,7 @@ lib_path_regex = re.compile('^(/usr(/X11R6)?)?/lib(64)?')
 lib_package_regex = re.compile('^(lib|.+-libs)')
 hidden_file_regex = re.compile('/\.[^/]*$')
 manifest_perl_regex = re.compile('^/usr/share/doc/perl-.*/MANIFEST(\.SKIP)?$')
-shebang_regex = re.compile(b'^#!\s*(\S+)(.*?)$', re.M)
+shebang_regex = re.compile(b'^#!\s*(\S+)([^\r\n]*?)$')
 interpreter_regex = re.compile('^/(?:usr/)?(?:s?bin|games|libexec(?:/.+)?|(?:lib(?:64)?|share)/.+)/([^/]+)$')
 script_regex = re.compile('^/((usr/)?s?bin|etc/(rc\.d/init\.d|X11/xinit\.d|cron\.(hourly|daily|monthly|weekly)))/')
 sourced_script_regex = re.compile('^/etc/(bash_completion\.d|profile\.d)/')
@@ -388,6 +388,12 @@ def python_bytecode_to_script(path):
         return res.group(1) + '.py'
 
     return None
+
+
+def script_interpreter(chunk):
+    res = shebang_regex.search(chunk) if chunk else None
+    return (b2s(res.group(1)), b2s(res.group(2)).strip()) \
+        if res else (None, "")
 
 
 class FilesCheck(AbstractCheck.AbstractCheck):
@@ -570,13 +576,7 @@ class FilesCheck(AbstractCheck.AbstractCheck):
                 if os.access(pkgfile.path, os.R_OK):
                     (chunk, istext) = peek(pkgfile.path, pkg)
 
-                interpreter = None
-                interpreter_args = ''
-                if chunk:
-                    res = shebang_regex.search(chunk)
-                    if res:
-                        interpreter = b2s(res.group(1))
-                        interpreter_args = b2s(res.group(2)).strip()
+                (interpreter, interpreter_args) = script_interpreter(chunk)
 
                 if doc_regex.search(f):
                     if not interpreter:

test/test_files.py
@@ -1,4 +1,5 @@
 from FilesCheck import python_bytecode_to_script as pbts
+from FilesCheck import script_interpreter as se
 
 
 class TestPythonBytecodeToScript(object):
@@ -15,5 +16,11 @@ class TestPythonBytecodeToScript(object):
         assert pbts("/usr/lib/python3.5/site-packages/__pycache__/pytest.cpython-35.opt-1.pyc") == "/usr/lib/python3.5/site-packages/pytest.py"
         assert pbts("/usr/lib/python3.5/site-packages/__pycache__/pytest.cpython-35.opt-2.pyc") == "/usr/lib/python3.5/site-packages/pytest.py"
         assert pbts("/usr/lib/python3.5/site-packages/__pycache__/pytest.cpython-35.pyc") == "/usr/lib/python3.5/site-packages/pytest.py"
+
+
+def test_script_interpreter():
+    assert se(b"#!/bin/bash -e") == ("/bin/bash", "-e")
+    assert se(b"#!/usr/bin/env python3 foo") == ("/usr/bin/env", "python3 foo")
+    assert se(b"# something here\n#!not a shebang") == (None, "")
 
 # ex: ts=4 sw=4 et

commit 3e4185de10f55e95a717cf34e85a6644ab9a688a

FilesCheck: Fix regression in finding shebangs without arguments
Was broken in fff597bbfba116c739d5cf15c60176ad99ddff66

FilesCheck.py
@@ -219,7 +219,7 @@ lib_path_regex = re.compile('^(/usr(/X11R6)?)?/lib(64)?')
 lib_package_regex = re.compile('^(lib|.+-libs)')
 hidden_file_regex = re.compile('/\.[^/]*$')
 manifest_perl_regex = re.compile('^/usr/share/doc/perl-.*/MANIFEST(\.SKIP)?$')
-shebang_regex = re.compile(b'^#!\s*(\S+)([^\r\n]*?)$')
+shebang_regex = re.compile(b'^#!\s*(\S+)(.*?)$', re.M)
 interpreter_regex = re.compile('^/(?:usr/)?(?:s?bin|games|libexec(?:/.+)?|(?:lib(?:64)?|share)/.+)/([^/]+)$')
 script_regex = re.compile('^/((usr/)?s?bin|etc/(rc\.d/init\.d|X11/xinit\.d|cron\.(hourly|daily|monthly|weekly)))/')
 sourced_script_regex = re.compile('^/etc/(bash_completion\.d|profile\.d)/')
@@ -395,7 +395,7 @@ def python_bytecode_to_script(path):
 def script_interpreter(chunk):
     res = shebang_regex.search(chunk) if chunk else None
     return (b2s(res.group(1)), b2s(res.group(2)).strip()) \
-        if res else (None, "")
+        if res and res.start() == 0 else (None, "")
 
 
 class FilesCheck(AbstractCheck.AbstractCheck):

test/test_files.py
@@ -19,7 +19,9 @@ class TestPythonBytecodeToScript(object):
 
 
 def test_script_interpreter():
-    assert se(b"#!/bin/bash -e") == ("/bin/bash", "-e")
+    assert se(b"#!/bin/sh\n# Hello world!\n") == ("/bin/sh", "")
+    assert se(b"#!/bin/bash -e\n") == ("/bin/bash", "-e")
+    assert se(b"#! /usr/bin/perl -wT \n") == ("/usr/bin/perl", "-wT")
     assert se(b"#!/usr/bin/env python3 foo") == ("/usr/bin/env", "python3 foo")
     assert se(b"# something here\n#!not a shebang") == (None, "")
 