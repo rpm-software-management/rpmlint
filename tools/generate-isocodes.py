5a369408038ca2656a2c97bc2727065ba20f2efb
Ville Skyttä <ville.skytta@iki.fi>
2016-04-21 18:44:18 +0300

    generate-isocodes: Switch to JSON source files (#72)

--- tools/generate-isocodes.py
+++ tools/generate-isocodes.py
@@ -3,38 +3,31 @@
 # Generate ISO codes for use with e.g. locale subdir checks
 # http://alioth.debian.org/projects/pkg-isocodes/
 
+import codecs
+import json
 import os
 from pprint import pprint
 import sys
 from urllib.request import urlopen
-from xml.etree.ElementTree import ElementTree
 
 
-iso_3166_url = os.environ.get("ISO_3166_URL", "http://anonscm.debian.org/cgit/pkg-isocodes/iso-codes.git/plain/iso_3166/iso_3166.xml")
-iso_639_url = os.environ.get("ISO_639_URL", "http://anonscm.debian.org/cgit/pkg-isocodes/iso-codes.git/plain/iso_639/iso_639.xml")
-iso_639_3_url = os.environ.get("ISO_639_3_URL", "http://anonscm.debian.org/cgit/pkg-isocodes/iso-codes.git/plain/iso_639_3/iso_639_3.xml")
+iso_3166_1_url = os.environ.get("ISO_3166_1_URL", "http://anonscm.debian.org/cgit/pkg-isocodes/iso-codes.git/plain/data/iso_3166-1.json")
+iso_639_3_url = os.environ.get("ISO_639_3_URL", "http://anonscm.debian.org/cgit/pkg-isocodes/iso-codes.git/plain/data/iso_639-3.json")
 
 langs = set()
 countries = set()
 
-# 2-letter country codes
-with urlopen(iso_3166_url) as f:
-    for entry in ElementTree(file=f).findall("iso_3166_entry"):
-        countries.add(entry.get("alpha_2_code"))
+# country codes (2 letters)
+with urlopen(iso_3166_1_url) as f:
+    data = json.load(codecs.getreader("utf-8")(f))
+    for entry in data["3166-1"]:
+        countries.add(entry["alpha_2"])
 
-# 2-letter codes
-with urlopen(iso_639_url) as f:
-    for entry in ElementTree(file=f).findall("iso_639_entry"):
-        for attr in ("iso_639_1_code", "iso_639_2T_code"):
-            code = entry.get(attr)
-            if code:
-                langs.add(code)
-                break
-
-# Remaining 2-letter codes plus 3-letter ones for which we have no 2-letter one
+# language codes (2 or 3 letters, 3 only for ones we don't have 2-letter one)
 with urlopen(iso_639_3_url) as f:
-    for entry in ElementTree(file=f).findall("iso_639_3_entry"):
-        langs.add(entry.get("part1_code") or entry.get("id"))
+    data = json.load(codecs.getreader("utf-8")(f))
+    for entry in data["639-3"]:
+        langs.add(entry.get("alpha_2") or entry["alpha_3"])
 
 # Note that we are not pprint()ing the set directly because with
 # Python 3 it results in curly brace set initializers that are not
