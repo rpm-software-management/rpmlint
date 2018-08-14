a4945df2d1de9f7b71a8ba403f9a9853c1b3332c
Tomáš Chvátal <tchvatal@suse.com>
2018-08-14 15:17:57 +0200

    Add copyright blurb to __init__.py wrt #165

--- rpmlint/__init__.py
+++ rpmlint/__init__.py
@@ -6,6 +6,25 @@ from rpmlint.Rpmdiff import Rpmdiff
 
 
 __version__ = '2.0.0'
+__copyright__ = """
+    Copyright (C) 2006 Mandriva
+    Copyright (C) 2009 Red Hat, Inc.
+    Copyright (C) 2009 Ville Skyttä
+    Copyright (C) 2017 SUSE LINUX GmbH
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
 
 
 def process_diff_args(argv):
