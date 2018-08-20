2b4594c95154955607ec741ef239cccfcac0bb3d
Tomáš Chvátal <tchvatal@suse.com>
2018-08-20 13:47:22 +0200

    Add byte_to_string function to replace b2s from Pkg

--- rpmlint/helpers.py
+++ rpmlint/helpers.py
@@ -8,3 +8,19 @@ def print_warning(message):
     Print warning message to stderr.
     """
     print(message, file=sys.stderr)
+
+
+def byte_to_string(item):
+    """
+    Convert byte items to strings
+    """
+    # empty stuff or already existing string stays
+    if item is None or isinstance(item, str):
+        return item
+
+    # if we have a list/tuple we have to recurse
+    if isinstance(item, (list, tuple)):
+        return [byte_to_string(i) for i in item]
+
+    # everything else shall be decoded and fails replaced
+    return item.decode(encoding='UTF-8', errors='replace')
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -10,3 +10,22 @@ def test_warnprint(capsys):
     out, err = capsys.readouterr()
     assert message not in out
     assert message in err
+
+
+def test_bytetostr():
+    """
+    Test bytetostr function
+    """
+    list_items = (
+        b'\xc5\xbe\xc3\xad\xc5\xbeala',
+        'texty',
+    )
+    item = b'p\xc5\x99\xc3\xad\xc5\xa1ern\xc4\x9b \xc5\xbelu\xc5\xa5ou\xc4\x8dk\xc3\xbd k\xc5\xaf\xc5\x88'
+
+    result = helpers.byte_to_string(item)
+    assert isinstance(result, str)
+    assert result == 'příšerně žluťoučký kůň'
+
+    result = helpers.byte_to_string(list_items)
+    assert isinstance(result, list)
+    assert result[0] == 'žížala'
