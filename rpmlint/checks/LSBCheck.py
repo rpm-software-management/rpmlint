ee1ee65587d41cf17814448e61e0e59aa3fd6e3b
Miro Hrončok <miro@hroncok.cz>
2019-03-10 12:03:33 +0100

    Remove over-indent (flake8 E117)

--- rpmlint/checks/LSBCheck.py
+++ rpmlint/checks/LSBCheck.py
@@ -30,11 +30,11 @@ class LSBCheck(AbstractCheck):
 
         version = pkg[rpm.RPMTAG_VERSION]
         if version and not version_regex.search(version):
-                self.output.add_info('E', pkg, 'non-lsb-compliant-version', version)
+            self.output.add_info('E', pkg, 'non-lsb-compliant-version', version)
 
         release = pkg[rpm.RPMTAG_RELEASE]
         if release and not version_regex.search(release):
-                self.output.add_info('E', pkg, 'non-lsb-compliant-release', release)
+            self.output.add_info('E', pkg, 'non-lsb-compliant-release', release)
 
 
 lsb_details_dict = {
