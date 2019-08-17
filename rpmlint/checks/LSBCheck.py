15e29230bc682ae2e455effe182c60ab6dd732f4
Kristýna Streitová <kstreitova@suse.com>
2019-08-18 00:47:37 +0200

    Separate LSBCheck to functions and add a testcase
    Separate the check() method to multiple functions and clean up the code. Also add a test for LSBCheck and adjust LSBCheck.toml description to be more precise (#225 #156).

--- rpmlint/checks/LSBCheck.py
+++ rpmlint/checks/LSBCheck.py
@@ -1,32 +1,51 @@
-#############################################################################
-# Project         : Mandriva Linux
-# Module          : rpmlint
-# File            : LSBCheck.py
-# Author          : Frederic Lepied
-# Created On      : Tue Jan 30 14:44:37 2001
-# Purpose         : LSB non compliance checks
-#############################################################################
-
 import re
 
 import rpm
 from rpmlint.checks.AbstractCheck import AbstractCheck
 
 
-version_regex = re.compile('^[a-zA-Z0-9.+]+$')
-name_regex = re.compile('^[a-z0-9.+-]+$')
-
-
 class LSBCheck(AbstractCheck):
+    """
+    Validate that package name, version and release number are LSB compliant.
+
+    The rules are the intersection of compatible NVRs between RPM v3 and DPKG
+    for supporting portability across RPM and Debian systems through tools
+    like alien.
+
+    Note: It uses values gained from rpm (RPMTAGs) not parsed from .rpm
+    filename.
+    """
+    name_regex = re.compile('^[a-z0-9.+-]+$')
+    version_regex = re.compile('^[a-zA-Z0-9.+]+$')
+
     def check(self, pkg):
+        self._check_lsb_name(pkg)
+        self._check_lsb_version(pkg)
+        self._check_lsb_release(pkg)
+
+    def _check_lsb_name(self, pkg):
+        """
+        Check if the package name is LSB compliant (only lowercase
+        letters, numbers, '.', '+' or '-' characters).
+        """
         name = pkg.name
-        if name and not name_regex.search(name):
-            self.output.add_info('E', pkg, 'non-lsb-compliant-package-name', name)
+        if name and not self.name_regex.search(name):
+            self.output.add_info('E', pkg, 'non-lsb-compliant-package-name', pkg.name)
 
+    def _check_lsb_version(self, pkg):
+        """
+        Check if the package version number is LSB compliant (only alphanumeric
+        symbols, '.' or '+' characters).
+        """
         version = pkg[rpm.RPMTAG_VERSION]
-        if version and not version_regex.search(version):
-            self.output.add_info('E', pkg, 'non-lsb-compliant-version', version)
+        if version and not self.version_regex.search(version):
+            self.output.add_info('E', pkg, 'non-lsb-compliant-version', pkg[rpm.RPMTAG_VERSION])
 
+    def _check_lsb_release(self, pkg):
+        """
+        Check if the package release number is LSB compliant (only alphanumeric
+        symbols, '.' or '+' characters).
+        """
         release = pkg[rpm.RPMTAG_RELEASE]
-        if release and not version_regex.search(release):
-            self.output.add_info('E', pkg, 'non-lsb-compliant-release', release)
+        if release and not self.version_regex.search(release):
+            self.output.add_info('E', pkg, 'non-lsb-compliant-release', pkg[rpm.RPMTAG_RELEASE])
