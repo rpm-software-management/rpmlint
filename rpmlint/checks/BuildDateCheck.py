cc5b4a2ee256763fe1ce9825b38579d2f8e3e46e
Martin Liska <mliska@suse.cz>
2019-08-27 11:05:53 +0200

    Add CheckBuildDate from openSUSE/rpmlint-checks (#227).

---
+++ rpmlint/checks/BuildDateCheck.py
@@ -0,0 +1,29 @@
+import re
+import stat
+import time
+
+from rpmlint.checks.AbstractCheck import AbstractFilesCheck
+
+
+class BuildDateCheck(AbstractFilesCheck):
+    def __init__(self, config, output):
+        super().__init__(config, output, r'.*')
+        self.looksliketime = re.compile('(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])')
+        self.istoday = re.compile(time.strftime('%b %e %Y'))
+
+    def check_file(self, pkg, filename):
+        if filename.startswith('/usr/lib/debug') or pkg.isSource():
+            return
+
+        if not stat.S_ISREG(pkg.files()[filename].mode):
+            return
+
+        grep_date = pkg.grep(self.istoday, filename)
+
+        if len(grep_date):
+            grep_time = pkg.grep(self.looksliketime, filename)
+
+            if len(grep_time):
+                self.output.add_info('E', pkg, 'file-contains-date-and-time', filename)
+            else:
+                self.output.add_info('E', pkg, 'file-contains-current-date', filename)
---
+++ test/test_build_date.py
@@ -0,0 +1,25 @@
+import re
+
+import pytest
+from rpmlint.checks.BuildDateCheck import BuildDateCheck
+from rpmlint.filter import Filter
+
+from Testing import CONFIG, get_tested_package
+
+
+@pytest.fixture(scope='function', autouse=True)
+def builddatecheck():
+    CONFIG.info = True
+    output = Filter(CONFIG)
+    test = BuildDateCheck(CONFIG, output)
+    return output, test
+
+
+@pytest.mark.parametrize('package', ['binary/builddate'])
+def test_build_date_time(tmpdir, package, builddatecheck):
+    output, test = builddatecheck
+    test.istoday = re.compile('Jan  1 2019')
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'E: file-contains-date-and-time /bin/with-datetime' in out
+    assert 'E: file-contains-current-date /bin/with-date' in out
