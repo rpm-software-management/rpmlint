8b7b69823a461dfd7280f8380da27186732f4d1f
Kristyna Streitova <kstreitova@suse.com>
2019-09-09 21:13:45 +0200

    Separate TmpFilesCheck to functions and add a test
    Separate the check() method to multiple methods and clean up the code.
    Also add a test (systemd-tmpfiles-1.0-0.x86_64.rpm) for an additional
    testing.
    
    Add a method that checks that "systemd-tmpfiles --create" call
    is not present in the %pre section as it should be in the %post.
    
    Remove "now check remaining ghost files that are not already" part
    as the same code is already present in PostCheck.py and it's not related
    to tmpfiles anyway. This means that "ghost-files-without-postin" and
    "postin-without-ghost-file-creation" checks were removed from
    TmpFilesCheck.py.
    
    Rename postin-without-tmpfile-creation to post-without-tmpfile-creation
    as we want to address %post section (there is no %postin).

--- test/test_tmp_files.py
+++ test/test_tmp_files.py
@@ -14,13 +14,24 @@ def tmpfilescheck():
 
 
 @pytest.mark.parametrize('package', ['binary/tempfiled'])
-def test_bashisms(tmpdir, package, tmpfilescheck):
+def test_tmpfiles(tmpdir, package, tmpfilescheck):
     output, test = tmpfilescheck
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
 
-    assert 'W: postin-without-tmpfile-creation /usr/lib/tmpfiles.d/krb5.conf' in out
-    assert 'W: postin-without-ghost-file-creation /tmp/foo' in out
-    assert 'W: tmpfile-not-ghost /var/lib/kerberos' in out
-    assert 'W: ghost-files-without-postin' in out
+    assert 'W: pre-with-tmpfile-creation ' not in out
+    assert 'W: post-without-tmpfile-creation /usr/lib/tmpfiles.d/krb5.conf' in out
+    assert 'W: tmpfile-not-in-filelist /var/lib/kerberos' in out
     assert 'W: tmpfile-not-regular-file /usr/lib/tmpfiles.d/symlink.conf' in out
+
+
+@pytest.mark.parametrize('package', ['binary/systemd-tmpfiles'])
+def test_tmpfiles2(tmpdir, package, tmpfilescheck):
+    output, test = tmpfilescheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+
+    assert 'W: pre-with-tmpfile-creation ' in out
+    assert 'W: post-without-tmpfile-creation ' not in out
+    assert 'W: tmpfile-not-in-filelist' not in out
+    assert 'W: tmpfile-not-regular-file' not in out
