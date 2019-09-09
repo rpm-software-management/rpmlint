09e418e898c05a858568e31e04f9131757258173
Kristyna Streitova <kstreitova@suse.com>
2019-09-09 22:55:39 +0200

    Separate PkgConfigCheck to methods
    Separate the check_file() method to multiple methods, add comments and
    clean up the code. Also use binary/libreiserfscore-devel-3.6.27-0.x86_64.rpm
    package that contains a correct .pc file for testing that the check
    doesn't print errors where it's not supposed to print.
    
    (#225 #156)

--- test/test_pkgconfig.py
+++ test/test_pkgconfig.py
@@ -21,3 +21,13 @@ def test_pkg_config(tmpdir, package, pkgconfigcheck):
     assert 'E: invalid-pkgconfig-file /tmp/pkgconfig/xcb.pc' in out
     assert 'E: pkgconfig-invalid-libs-dir /tmp/pkgconfig/xcb.pc Libs: -L/usr/lib' in out
     assert 'E: double-slash-in-pkgconfig-path /tmp/pkgconfig/xcb.pc includedir=/usr/include//xyz' in out
+
+
+@pytest.mark.parametrize('package', ['binary/libreiserfscore-devel'])
+def test_pkg_config_correct(tmpdir, package, pkgconfigcheck):
+    output, test = pkgconfigcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'E: invalid-pkgconfig-file' not in out
+    assert 'E: pkgconfig-invalid-libs-dir' not in out
+    assert 'E: double-slash-in-pkgconfig-path' not in out
