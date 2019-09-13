5e3e9f7b58ca66e511089cedde75888b28a5dd63
Kristyna Streitova <kstreitova@suse.com>
2019-09-13 20:28:41 +0200

    Rename ExecDocCheck to DocCheck
    Rename as it's no longer just a check that searches for the
    executable documentation.
    
    Add comments, rename test method (typo) and remove
    "package-with-huge-translation" from .toml as it's not used anymore.

--- rpmlint/checks/DocCheck.py
+++ rpmlint/checks/DocCheck.py
@@ -4,22 +4,30 @@ import stat
 from rpmlint.checks.AbstractCheck import AbstractCheck
 
 
-class ExecDocCheck(AbstractCheck):
-    @staticmethod
-    def ignore_pkg(name):
-        if name.startswith('bundle-') or '-devel' in name or '-doc' in name:
-            return True
-        return False
+class DocCheck(AbstractCheck):
+    """
+    Checks for package documentation.
+    """
 
     def check(self, pkg):
         if pkg.isSource():
             return
 
         self._check_executable_docs(pkg)
-        if not self.ignore_pkg(pkg.name):
+        if not self._ignore_pkg(pkg.name):
             self._check_huge_docs(pkg)
 
+    @staticmethod
+    def _ignore_pkg(name):
+        if name.startswith('bundle-') or '-devel' in name or '-doc' in name:
+            return True
+        return False
+
     def _check_executable_docs(self, pkg):
+        """
+        Check if the documentation in the package is executable and print an
+        error if it is.
+        """
         files = pkg.files()
         for f in pkg.docFiles():
             mode = files[f].mode
@@ -35,6 +43,10 @@ class ExecDocCheck(AbstractCheck):
                     self.output.add_info('E', pkg, 'executable-docs', f)
 
     def _check_huge_docs(self, pkg):
+        """
+        Check the size of the documentation in the package and print a warning
+        if it's more than half of the size of the package.
+        """
         files = pkg.files()
         complete_size = 0
         for _, pkgfile in files.items():
