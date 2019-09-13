36e7c403051ec263be553fd8c5dad748b787390e
Kristýna Streitová <kstreitova@42ka.suse.cz>
2019-09-13 13:03:31 +0200

    Small cleanup, comments and .toml rename
    Rename CheckBuildDate.toml to BuildDateCheck.toml so it corresponds
    with the check name. Also, add a test case that tests if it doesn't
    print errors when it shouldn't.

--- rpmlint/checks/BuildDateCheck.py
+++ rpmlint/checks/BuildDateCheck.py
@@ -6,16 +6,19 @@ from rpmlint.checks.AbstractCheck import AbstractFilesCheck
 
 
 class BuildDateCheck(AbstractFilesCheck):
+    """
+    Check that the file doesn't contain the current date or time.
+
+    If so, it causes the package to rebuild when it's not needed.
+    """
     def __init__(self, config, output):
         super().__init__(config, output, r'.*')
         self.looksliketime = re.compile('(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])')
         self.istoday = re.compile(time.strftime('%b %e %Y'))
 
     def check_file(self, pkg, filename):
-        if filename.startswith('/usr/lib/debug') or pkg.isSource():
-            return
-
-        if not stat.S_ISREG(pkg.files()[filename].mode):
+        if filename.startswith('/usr/lib/debug') or pkg.isSource() or \
+                not stat.S_ISREG(pkg.files()[filename].mode):
             return
 
         grep_date = pkg.grep(self.istoday, filename)
