a3cde028c836e0b602d4ef11cba7fcb7ff378927
Kristyna Streitova <kstreitova@suse.com>
2019-09-30 16:31:34 +0200

    DuplicatesCheck.py: clean up and add comments
    Make a cleanup and add comments so the algorithm makes a little bit
    more sense. Add a test case that handles a correct package with no
    duplicates errors.

--- rpmlint/checks/DuplicatesCheck.py
+++ rpmlint/checks/DuplicatesCheck.py
@@ -5,12 +5,19 @@ from rpmlint.checks.AbstractCheck import AbstractCheck
 
 
 class DuplicatesCheck(AbstractCheck):
-    @staticmethod
-    def get_prefix(filename):
-        pathlist = str.split(filename, '/')
-        if len(pathlist) == 3:
-            return '/'.join(pathlist[0:2])
-        return '/'.join(pathlist[0:3])
+    """
+    Search for the duplicated files in the package.
+
+    It uses the following structures:
+
+    md5s
+    - key: md5 hash of the file
+    - values: files with this hash
+
+    sizes
+    - key: md5 hash of the file
+    - values: size of the file
+    """
 
     def check(self, pkg):
         if pkg.isSource():
@@ -18,49 +25,77 @@ class DuplicatesCheck(AbstractCheck):
 
         md5s = {}
         sizes = {}
-        files = pkg.files()
-        configFiles = pkg.configFiles()
+        total_dup_size = 0
 
-        for f, pkgfile in files.items():
-            if f in pkg.ghostFiles():
+        for fname, pkgfile in pkg.files().items():
+            if fname in pkg.ghostFiles() or not stat.S_ISREG(pkgfile.mode):
                 continue
 
-            if not stat.S_ISREG(pkgfile.mode):
-                continue
-
-            md5s.setdefault(pkgfile.md5, set()).add(f)
+            # fillup md5s and sizes dicts
+            md5s.setdefault(pkgfile.md5, set()).add(fname)
             sizes[pkgfile.md5] = pkgfile.size
 
-        total = 0
-        for f in md5s:
-            duplicates = md5s[f]
+        # process duplicates
+        for md5_hash in md5s:
+            # obtain the list of the files with the same md5 hash
+            duplicates = md5s[md5_hash]
+
+            # continue, there is no duplicate
             if len(duplicates) == 1:
                 continue
 
             duplicates = sorted(list(duplicates))
             first = duplicates.pop()
             first_is_config = False
-            if first in configFiles:
+            if first in pkg.configFiles():
                 first_is_config = True
 
-            partition = self.get_prefix(first)
+            prefix = self._get_prefix(first)
 
             st = os.stat(pkg.dirName() + '/' + first)
+            # 1 (first) + number of others - number of hard links
+            # (keeps track of how many directories have entries for this file)
+            # diff is a number of files that are duplicates but not hard-links
             diff = 1 + len(duplicates) - st[stat.ST_NLINK]
+
             if diff <= 0:
+                # now we have just hard-links in duplicates
                 for duplicate in duplicates:
-                    if partition != self.get_prefix(duplicate):
-                        self.output.add_info('E', pkg, 'hardlink-across-partition', first, duplicate)
-                    if first_is_config and duplicate in configFiles:
-                        self.output.add_info('E', pkg, 'hardlink-across-config-files', first, duplicate)
+                    if prefix != self._get_prefix(duplicate):
+                        self.output.add_info('E', pkg,
+                                             'hardlink-across-partition',
+                                             first, duplicate)
+                    if first_is_config and duplicate in pkg.configFiles():
+                        self.output.add_info('E', pkg,
+                                             'hardlink-across-config-files',
+                                             first, duplicate)
                 continue
 
+            # now we know that there are some duplicates that are not links
             for duplicate in duplicates:
-                if partition != self.get_prefix(duplicate):
+                if prefix != self._get_prefix(duplicate):
+                    # if the duplicate is in a different prefix, we can ignore
+                    # it since it can't be linked anyway
                     diff = diff - 1
-            total += sizes[f] * diff
-            if sizes[f] and diff > 0:
-                self.output.add_info('W', pkg, 'files-duplicate', first, ':'.join(duplicates))
 
-        if total > 100000:
-            self.output.add_info('E', pkg, 'files-duplicated-waste', total)
+            # if there is still a positive diff (i.e. there is a duplicate that
+            # is not a link and wasn't ignored by the previous step),
+            # report a warning
+            if sizes[md5_hash] and diff > 0:
+                self.output.add_info('W', pkg, 'files-duplicate', first,
+                                     ':'.join(duplicates))
+            total_dup_size += sizes[md5_hash] * diff
+
+        # check the overall size of the duplicates and print an error if it's
+        # too much
+        if total_dup_size > 100000:
+            self.output.add_info('E', pkg, 'files-duplicated-waste',
+                                 total_dup_size)
+
+    @staticmethod
+    def _get_prefix(filename):
+        """Return first two directories in the given path."""
+        pathlist = str.split(filename, '/')
+        if len(pathlist) == 3:
+            return '/'.join(pathlist[0:2])
+        return '/'.join(pathlist[0:3])
--- test/test_duplicates.py
+++ test/test_duplicates.py
@@ -18,7 +18,21 @@ def test_duplicates(tmpdir, package, duplicatescheck):
     output, test = duplicatescheck
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
+
     assert 'E: hardlink-across-partition /var/foo /etc/foo' in out
     assert 'E: hardlink-across-config-files /var/foo2 /etc/foo2' in out
     assert 'W: files-duplicate /etc/bar3 /etc/bar:/etc/bar2' in out
+    assert 'W: files-duplicate /etc/strace2.txt /etc/strace1.txt' in out
     assert 'E: files-duplicated-waste 270516' in out
+
+
+@pytest.mark.parametrize('package', ['binary/bad-crc-uncompressed'])
+def test_duplicates_correct(tmpdir, package, duplicatescheck):
+    output, test = duplicatescheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+
+    assert 'E: hardlink-across-partition' not in out
+    assert 'E: hardlink-across-config-files' not in out
+    assert 'W: files-duplicate' not in out
+    assert 'E: files-duplicated-waste' not in out
