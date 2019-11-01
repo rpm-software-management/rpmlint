4141e5019e9fb262bd4c492fd6cb5cdc0edffac0
Tomáš Chvátal <tchvatal@suse.com>
2019-11-01 11:42:09 +0100

    Add check for shadowed initscript.

--- test/test_sysvinitonsystemd.py
+++ test/test_sysvinitonsystemd.py
@@ -19,5 +19,13 @@ def test_sysv_init_on_systemd_check(tmpdir, package, sysvcheck):
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
     assert 'E: obsolete-insserv-requirement' in out
-    assert 'E: deprecated-init-script /etc/init.d/weekly.script' in out
-    assert 'E: deprecated-boot-script /etc/init.d/boot.script' in out
+    assert 'E: deprecated-init-script weekly.script' in out
+    assert 'E: deprecated-boot-script boot.script' in out
+
+
+@pytest.mark.parametrize('package', ['binary/rc-links'])
+def test_overshadowing_of_initscript(tmpdir, package, sysvcheck):
+    output, test = sysvcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'E: systemd-shadowed-initscript bar' in out
