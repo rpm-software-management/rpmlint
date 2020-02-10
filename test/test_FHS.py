c3d0ad17cc9c10a5de1ccd64e8d65fc4f9ba6a38
Markéta Calábková <meggy.calabkova@gmail.com>
2020-02-10 13:31:53 +0100

    Separate FHSCheck to functions and add a testcase rpm-software-management#225, rpm-software-management#156

---
+++ test/test_FHS.py
@@ -0,0 +1,35 @@
+import pytest
+from rpmlint.checks.FHSCheck import FHSCheck
+from rpmlint.filter import Filter
+
+from Testing import CONFIG, get_tested_package
+
+
+@pytest.fixture(scope='function', autouse=True)
+def fhscheck():
+    CONFIG.info = True
+    output = Filter(CONFIG)
+    test = FHSCheck(CONFIG, output)
+    return output, test
+
+
+@pytest.mark.parametrize('package', ['binary/non-fhs'])
+def test_FHS_compliance(tmpdir, package, fhscheck):
+    """
+    Check that the directories are not FHS compliant.
+    """
+    output, test = fhscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+
+    # Check invalid /usr subdirectory
+    assert 'non-standard-dir-in-usr' in out
+    assert 'sbin' in out
+
+    # Check illegal FSSTND subdirectory
+    assert 'FSSTND-dir-in-var' in out
+    assert 'catman' in out
+
+    # Check invalid /var subdirectory
+    assert 'non-standard-dir-in-var' in out
+    assert 'lib' in out
