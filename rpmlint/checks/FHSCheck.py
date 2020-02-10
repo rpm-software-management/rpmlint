c70528083f80b5772f64ae3fa9abadf18613ae2b
Markéta Calábková <meggy.calabkova@gmail.com>
2020-02-10 13:31:53 +0100

    Separate FHSCheck to functions and add a testcase rpm-software-management#225, rpm-software-management#156

--- rpmlint/checks/FHSCheck.py
+++ rpmlint/checks/FHSCheck.py
@@ -1,21 +1,16 @@
-#############################################################################
-# File          : FHSCheck.py
-# Package       : rpmlint
-# Author        : Frederic Lepied
-# Created on    : Fri Oct 15 17:40:32 1999
-# Purpose       : check FHS conformity
-#############################################################################
-
 import re
 
 from rpmlint.checks.AbstractCheck import AbstractCheck
 
 
 class FHSCheck(AbstractCheck):
-    usr_regex = re.compile('^/usr/([^/]+)/')
+    """
+    Validate that binary files are packaged according to FHS.
+    """
+    usr_regex = re.compile('^/usr/([^/]+)')
     usr_subdir = ('X11R6', 'bin', 'games', 'include', 'lib', 'lib64',
                   'local', 'sbin', 'share', 'src', 'tmp')
-    var_regex = re.compile('^/var/([^/]+)/')
+    var_regex = re.compile('^/var/([^/]+)')
     var_fsstnd = ('adm', 'catman', 'local', 'named', 'nis', 'preserve')
     var_subdir = ('account', 'cache', 'crash', 'games', 'lib', 'lock', 'log',
                   'mail', 'opt', 'run', 'spool', 'tmp', 'yp', 'www', 'ftp')
@@ -32,21 +27,46 @@ class FHSCheck(AbstractCheck):
             s = FHSCheck.usr_regex.search(fname)
             if s:
                 d = s.group(1)
-                if d not in FHSCheck.usr_subdir and d not in usr_list:
+                if not self._check_usr_std_dir(d, usr_list):
                     self.output.add_info('W', pkg, 'non-standard-dir-in-usr', d)
-                    usr_list.append(d)
             else:
                 s = FHSCheck.var_regex.search(fname)
                 if s:
                     d = s.group(1)
-                    if d in var_list:
-                        continue
-                    if d in FHSCheck.var_fsstnd:
-                        self.output.add_info('W', pkg, 'FSSTND-dir-in-var', fname)
-                        var_list.append(d)
-                    elif d not in FHSCheck.var_subdir:
+                    if not self._check_var_FSSTND_dir(d, var_list):
+                        self.output.add_info('W', pkg, 'FSSTND-dir-in-var', d)
+                    elif not self._check_var_std_dir(s, var_list):
                         self.output.add_info('W', pkg, 'non-standard-dir-in-var', d)
-                        var_list.append(d)
+
+    @staticmethod
+    def _check_usr_std_dir(d, usr_list):
+        """
+        Check if the file is in valid subdirectory of /usr.
+        """
+        if d not in FHSCheck.usr_subdir and d not in usr_list:
+            usr_list.append(d)
+            return False
+        return True
+
+    @staticmethod
+    def _check_var_FSSTND_dir(d, var_list):
+        """
+        Check if the file is not packaged in an obsolete (illegal) subdirectory of /var.
+        """
+        if d in FHSCheck.var_fsstnd and d not in var_list:
+            var_list.append(d)
+            return False
+        return True
+
+    @staticmethod
+    def _check_var_std_dir(d, var_list):
+        """
+        Check if the file is in valid subdirectory of /var.
+        """
+        if d not in FHSCheck.var_subdir and d not in var_list:
+            var_list.append(d)
+            return False
+        return True
 
 
 fhs_details_dict = {
