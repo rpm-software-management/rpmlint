cefda4d1daaf9ed867a1246e2abbea659a133e21
Markéta Calábková <meggy.calabkova@gmail.com>
2020-02-13 10:30:03 +0100

    tests from rpmlint-tests for MenuXDG (rpm-software-management#156)

--- test/test_menuxdg.py
+++ test/test_menuxdg.py
@@ -22,3 +22,46 @@ def test_raises_parse_error(tmpdir, package, menuxdgcheck):
     assert 'contains parsing error' in out
     assert ' invalid-desktopfile ' in out
     assert 'check with desktop-file-validate' in out
+
+
+@pytest.mark.parametrize('package', ['binary/desktopfile-bad-binary'])
+def test_without_binary(tmpdir, package, menuxdgcheck):
+    output, test = menuxdgcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'desktopfile-without-binary' in out
+
+
+@pytest.mark.parametrize('package', ['binary/desktopfile-bad-duplicate'])
+def test_duplicate(tmpdir, package, menuxdgcheck):
+    output, test = menuxdgcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'desktopfile-duplicate-section' in out
+    assert 'invalid-desktopfile' in out
+
+
+@pytest.mark.parametrize('package', ['binary/desktopfile-bad-section'])
+def test_missing_header(tmpdir, package, menuxdgcheck):
+    output, test = menuxdgcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'desktopfile-missing-header' in out
+    assert 'invalid-desktopfile' in out
+
+
+@pytest.mark.parametrize('package', ['binary/desktopfile-bad-unicode'])
+def test_bad_unicode(tmpdir, package, menuxdgcheck):
+    output, test = menuxdgcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'invalid-desktopfile' in out
+    assert 'non-utf8-desktopfile' in out
+
+
+@pytest.mark.parametrize('package', ['binary/desktopfile-good'])
+def test_good(tmpdir, package, menuxdgcheck):
+    output, test = menuxdgcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert not out
