f531b575375c90bfc1989eec812748f344029525
Tomáš Chvátal <tchvatal@suse.com>
2020-03-30 19:53:11 +0200

    We support python 3.8

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Utilities',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: System :: Archiving :: Packaging',
