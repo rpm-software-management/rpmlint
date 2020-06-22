de57be35a31bd8e88a8be938f81530f5a594e951
Malte Kraus <malte.kraus@suse.com>
2020-06-22 14:44:57 +0200

    port over lint to enforce a D-Bus service allow list

---
+++ test/test_dbus_services.py
@@ -0,0 +1,32 @@
+import pytest
+from rpmlint.checks.CheckDBUSServices import DBUSServiceCheck
+from rpmlint.filter import Filter
+
+from Testing import CONFIG, get_tested_package
+
+
+@pytest.fixture(scope='function', autouse=True)
+def dbusservicescheck(tmpdir):
+    CONFIG.info = True
+    CONFIG.configuration['DBUSServices.WhiteList'] = ('1', '2', '3')
+
+    output = Filter(CONFIG)
+    test = DBUSServiceCheck(CONFIG, output)
+
+    return output, test
+
+
+@pytest.mark.parametrize('package', ['binary/dbus_svc'])
+def test_dbus_services(tmpdir, package, dbusservicescheck):
+    output, test = dbusservicescheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'noproblem' not in out
+    # these are allowed and should NOT produce errors
+    assert 'suse-dbus-unauthorized-service /usr/share/dbus-1/system-services/1' not in out
+    assert 'suse-dbus-unauthorized-service /usr/share/dbus-1/system.d/2' not in out
+    assert 'suse-dbus-unauthorized-service /etc/dbus-1/system.d/3' not in out
+    # these are not allowed and should produce errors
+    assert 'suse-dbus-unauthorized-service /usr/share/dbus-1/system-services/a' in out
+    assert 'suse-dbus-unauthorized-service /usr/share/dbus-1/system.d/b' in out
+    assert 'suse-dbus-unauthorized-service /etc/dbus-1/system.d/c' in out
