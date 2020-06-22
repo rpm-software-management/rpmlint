1e15751632a73e4567a2cc85e243805905647e62
Malte Kraus <malte.kraus@suse.com>
2020-06-22 14:46:28 +0200

    port over lint to enforce a polkit rule allow list

---
+++ rpmlint/checks/PolkitRulesAllowedCheck.py
@@ -0,0 +1,19 @@
+from .Allowlisting import AbstractAllowlistCheck
+
+
+class PolkitRulesAllowedCheck(AbstractAllowlistCheck):
+    """ Checks that polkit rules files are on an allow list. """
+
+    restricted_paths = (
+        '/etc/polkit-1/rules.d/',
+        '/usr/share/polkit-1/rules.d/',
+    )
+    error_map = {
+        'unauthorized': 'polkit-unauthorized-rules',
+        'changed': 'polkit-changed-rules',
+        'ghost': 'polkit-ghost-file',
+    }
+
+    @property
+    def allowlist_filenames(self):
+        return self.config.configuration.get('PolkitRulesWhitelist', ())
---
+++ test/test_polkit_allowlist.py
@@ -0,0 +1,40 @@
+import json
+import os
+import os.path
+
+import pytest
+from rpmlint.checks.PolkitRulesAllowedCheck import PolkitRulesAllowedCheck
+from rpmlint.filter import Filter
+
+from Testing import CONFIG, get_tested_package
+
+
+@pytest.fixture(scope='function', autouse=True)
+def polkit_rules_allowed_check(tmpdir):
+    CONFIG.info = True
+
+    # make the check search for its allow list in 'tmpdir'
+    CONFIG.configuration['WhitelistDataDir'] = [tmpdir]
+    CONFIG.configuration['PolkitRulesWhitelist'] = ('allowed-polkit-rules.json',)
+
+    output = Filter(CONFIG)
+    test = PolkitRulesAllowedCheck(CONFIG, output)
+
+    # make an empty allow list
+    with open(os.path.join(tmpdir, 'allowed-polkit-rules.json'), 'w') as fd:
+        json.dump({'polkit-rules-test': {'audits': {'bsc#0': {'comment': '', 'digests': {
+            '/usr/share/polkit-1/rules.d/regular': 'sha256:invalid'
+        }}}}}, fd)
+
+    return output, test
+
+
+@pytest.mark.parametrize('package', ['binary/polkit-rules-test'])
+def test_polkit_privs(tmpdir, package, polkit_rules_allowed_check):
+    output, test = polkit_rules_allowed_check
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'noproblem' not in out
+    assert 'polkit-changed-rules /usr/share/polkit-1/rules.d/regular' in out
+    assert 'polkit-unauthorized-rules /etc/polkit-1/rules.d/regular' in out
+    assert 'polkit-ghost-file /usr/share/polkit-1/rules.d/ghost' in out
