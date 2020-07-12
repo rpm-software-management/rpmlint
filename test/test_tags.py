17d23330c03179552782453824d7db6bcb6b1f53
thisisshub <thisisshub@gmail.com>
2020-07-12 21:06:19 +0530

    Add tests in test_tags & modify TagsCheck.toml
    This commit contains tests for checks,
    - description-line-too-long
    - description-shorter-than-summary
    - invalid-dependency
    - invalid-url
    - invalid-version
    - name-repeated-in-summary
    - no-description-tag
    - obsolete-not-provided
    - self-obsoletion
    - summary-ended-with-dot
    - summary-has-leading-spaces
    - summary-not-capitalized
    - summary-too-long
    - tag-in-description
    - unexpanded-macro
    - unreasonable-epoch

--- test/test_tags.py
+++ test/test_tags.py
@@ -103,3 +103,195 @@ def test_shlib2_devel(tmpdir, package, tagscheck):
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
     assert 'W: no-dependency-on' in out
+
+
+@pytest.mark.parametrize('package', ['binary/unexpanded-macro-exp'])
+def test_check_unexpanded_macro(tmpdir, package, tagscheck):
+    """Test if a package has an unexpanded macro in it's specfile."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert 'W: unexpanded-macro Packager %ppc' in out
+    assert 'W: unexpanded-macro Group %ppc' in out
+    assert 'W: unexpanded-macro Provides /something%ppc %ppc' in out
+    assert 'W: unexpanded-macro Conflicts something:%ppc %ppc' in out
+    assert 'W: unexpanded-macro Supplements packageand(python-gobject:%{gdk_real_package})%ppc %ppc' in out
+    assert 'W: unexpanded-macro Suggests /%ppc %ppc' in out
+    assert 'W: unexpanded-macro Enhances /%ppc %ppc' in out
+    assert 'W: unexpanded-macro Recommends /%ppc %ppc' in out
+    assert 'W: unexpanded-macro Supplements packageand(python-gobject:%{gdk_real_package})%ppc %{gdk_real_package}' in out
+    assert 'W: unexpanded-macro Supplements packageand(python-gobject:%{gdk_real_package})%ppc %ppc' in out
+
+
+@pytest.mark.parametrize('package', ['binary/invalid-version'])
+def test_check_errors(tmpdir, package, tagscheck):
+    """Test package for check invalid-version."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package has a Version: tag with pre/alpha/beta suffixes in it's specfile
+    assert 'E: invalid-version 0pre' in out
+
+
+# TODO: Add a test for no-changelogname-tag not in out
+
+
+@pytest.mark.parametrize('package', ['binary/summary-warning'])
+def test_check_summary_warning(tmpdir, package, tagscheck):
+    """Test package for check
+    - in out,
+        summary-too-long, summary-has-leading-spaces,
+        description-shorter-than-summary,
+    - not in out
+    invalid-version, unexpanded-macro.
+    """
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if package has a summary longer than 80 characters
+    assert 'E: summary-too-long C   lorem Ipsum is simply dummy text of the printing and typesetting industry' in out
+    # Test if package has leading space at the beginning of the summary
+    assert 'E: summary-has-leading-spaces C   lorem Ipsum is simply dummy text of the printing and typesetting industry' in out
+    # Test if package has a shorter description than Summary
+    assert 'W: description-shorter-than-summary' in out
+    # Test if a package has a Version: tag
+    # without any pre/alpha/beta suffixes in it's specfile.
+    assert 'E: invalid-version' not in out
+    # Test if a package does not have an unexpanded
+    # macro in it's specfile.
+    assert 'W: unexpanded-macro' not in out
+
+
+@pytest.mark.parametrize('package', ['binary/no-url-tag'])
+def test_check_warning(tmpdir, package, tagscheck):
+    """Test if a package contains the warning for
+    summary-not-capitalized, summary-ended-with-dot,
+    no-url-tag."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if package Summary does not start with a capital letter
+    assert 'W: summary-not-capitalized C no-url-tag warning.' in out
+    # Test if a package Summary ends with a period
+    assert 'W: summary-ended-with-dot C no-url-tag warning.' in out
+    # Test if a package does not contain a Url: tag
+    assert 'W: no-url-tag' in out
+
+
+@pytest.mark.parametrize('package', ['binary/invalid-la-file'])
+def test_check_errors_not_found(tmpdir, package, tagscheck):
+    """Test packages for checks
+    summary-too-long, summary-not-capitalized,
+    summary-ended-with-dot, summary-has-leading-spaces,
+    no-url-tag, description-shorter-than-summary.
+    """
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package Summary is not longer than 80 characters
+    assert 'W: summary-too-long' not in out
+    # Test if Summary starts with a capitalized word
+    assert 'W: summary-not-capitalized' not in out
+    # Test if a package Summary does not end with a period
+    assert 'W: summary-ended-with-dot' not in out
+    # Test if a package Summary does not begin with a leading space
+    assert 'W: summary-has-leading-spaces' not in out
+    # Test if a package contains a Url: tag
+    assert 'W: no-url-tag' not in out
+    # Test if package no shorter description than Summary
+    assert 'W: description-shorter-than-summary' not in out
+
+
+@pytest.mark.parametrize('package', ['binary/misc-warnings'])
+def test_check_misc_warning(tmpdir, package, tagscheck):
+    """Test package for check tag-in-description,
+    name-repeated-in-summary, invalid-url."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package has a tag such as Name: in the description
+    assert 'W: tag-in-description C Name:' in out
+    # Test if a package has it's name repeated in the summary tag
+    assert 'W: name-repeated-in-summary C misc-warnings' in out
+    # Test if a package has a invalid url value in the URL: tag
+    assert 'W: invalid-url URL so;mething.' in out
+
+
+@pytest.mark.parametrize('package', ['binary/misc'])
+def test_check_misc_warning_not_found(tmpdir, package, tagscheck):
+    """Test package for check not in out
+    tag-in-description, name-repeated-in-summary, invalid-url."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package does not have a tag in description
+    assert 'W: tag-in-description' not in out
+    # Test if a package does not have a name repeated in the summary tag value
+    assert 'W: name-repeated-in-summary' not in out
+    # Test if a package does not have a invalid url in the URL tag value
+    assert 'W: invalid-url' not in out
+
+
+@pytest.mark.parametrize('package', ['binary/invalid-dependency'])
+def test_check_invalid_dependency(tmpdir, package, tagscheck):
+    """Test if a package has
+    invalid-dependency, no-description-tag, unreasonable-epoch."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package has a Epoch tag value greater than 99
+    assert 'W: unreasonable-epoch 100' in out
+    # Test if a package has Requires: starts with /usr/local
+    assert 'E: invalid-dependency /usr/local/something' in out
+    # Test if a package has empty or no description tag
+    assert 'E: no-description-tag' in out
+
+
+@pytest.mark.parametrize('package', ['binary/random-exp'])
+def test_package_random_warnings(tmpdir, package, tagscheck):
+    """Test if a package has check,
+    - in out,
+        obsolete-not-provided
+        description-line-too-long,
+    - not in out,
+        invalid-dependency,
+        unreasonable-epoch,
+        no-description-tag,
+        self-obsoletion."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package that was obsoleted is still provided
+    # in newer package to avoid unnecessary dependency breakage
+    assert 'W: obsolete-not-provided' in out
+    # Test if a package has a description longer than 79 characters
+    assert 'E: description-line-too-long C This is ridiculously long description that has no meaning but is used to test the check description-line-too-long.' in out
+    # Test if a package has a Requires tag value start with
+    # string other than /usr/local/
+    assert 'E: invalid-dependency' not in out
+    # Test if a package has a Epoch tag value less than or equal to 99
+    assert 'W: unreasonable-epoch' not in out
+    # Test if a package has a non-empty description tag
+    assert 'W: no-description-tag' not in out
+    # Test if a package does not have itself in Obsoletes: tag value
+    assert 'W: self-obsoletion' not in out
+
+
+@pytest.mark.parametrize('package', ['binary/random-devel'])
+def test_package_random_exp(tmpdir, package, tagscheck):
+    """Test if a package check,
+    - in out,
+        self-obsoletion,
+    - not in out,
+        obsolete-not-provided,
+        description-line-too-long."""
+    output, test = tagscheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    # Test if a package obsoletes itself i.e. Obsoletes: random-devel
+    assert 'W: self-obsoletion random-devel obsoletes random-devel' in out
+    # Test if a obsoleted package has been provided to
+    # to avoid unnecessary breakage i.e. Requires: python
+    assert 'W: obsolete-not-provided' not in out
+    # Test if a package has a description line not greater 79 characters
+    assert 'E: description-line-too-long' not in out
