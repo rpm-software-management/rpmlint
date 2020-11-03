19f97a4a3a35187d3ab8f458c3de5774e1361b67
Martin Liska <mliska@suse.cz>
2020-11-03 13:59:05 +0100

    Add --permissive by default for now.
    It's a temporary commit before we will have
    a full OBS itegration.

--- rpmlint/cli.py
+++ rpmlint/cli.py
@@ -172,7 +172,9 @@ def lint():
     """
     Main wrapper for lint command processing
     """
-    options = process_lint_args(sys.argv[1:])
+
+    # TODO: remove once OBS integration is done
+    options = process_lint_args(sys.argv[1:] + ['--permissive'])
 
     lint = Lint(options)
     sys.exit(lint.run())
