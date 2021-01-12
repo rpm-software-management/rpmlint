142e9c5b85a2685ca4ab478f9eb9c0f69b6aa630
Martin Liska <mliska@suse.cz>
2021-01-12 14:53:32 +0100

    Add 2 FileMetadataCheck checks.

---
+++ test/test_world_writable.py
@@ -0,0 +1,26 @@
+import pytest
+from rpmlint.checks.WorldWritableCheck import WorldWritableCheck
+from rpmlint.filter import Filter
+from rpmlint.pkg import FakePkg
+
+from Testing import CONFIG
+
+
+@pytest.fixture(scope='function', autouse=True)
+def check():
+    CONFIG.info = True
+    output = Filter(CONFIG)
+    test = WorldWritableCheck(CONFIG, output)
+    return output, test
+
+
+def test_device_files_simple(check):
+    output, test = check
+    with FakePkg('dummy') as pkg:
+        pkg.add_file_with_content('/tmp', 'temp folder', rdev=0, mode=17389)
+        pkg.add_file_with_content('/tempus', 'tempus', rdev=0, mode=17389)
+        test.check(pkg)
+    out = output.print_results(output.results)
+    assert len(output.results) == 2
+    assert 'dummy: E: world-writable-mismatched-attrs /tempus expected "mode": drw-rw---t, has: drwxr-xr-t'
+    assert 'dummy: E: world-writable-unauthorized-file /tmp' in out
