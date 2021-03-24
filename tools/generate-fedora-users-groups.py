5ee8dc832a1d5c4ba3c2f5f008991fa8c15dafea
Miroslav Suchý <msuchy@redhat.com>
2021-03-24 12:43:37 +0100

    extract script generating Fedora users and groups to tools/

---
+++ tools/generate-fedora-users-groups.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This sciptt is used to generate values for
+    config/Fedora/configs/Fedora/users-groups.toml
+"""
+import os
+import re
+users = set()
+groups = set()
+uidgid_regex = re.compile(r'^\s*(\S+)\s+(-|\d+)\s+(-|\d+|\(\d+\))\s')
+for uidgid_file in ['/usr/share/doc/setup/uidgid']:
+    if os.path.exists(uidgid_file):
+        fobj = open(uidgid_file)
+        try:
+            for line in fobj.read().strip().splitlines():
+                res = uidgid_regex.search(line)
+                if res:
+                    name = res.group(1)
+                    if res.group(2) != '-':
+                        users.add(name)
+                    if res.group(3) != '-' and '(' not in res.group(3):
+                        groups.add(name)
+                del res
+            del line
+        finally:
+            fobj.close()
+        del fobj
+
+print(users)
+
+print(groups)
