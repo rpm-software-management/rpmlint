8b915d2e075526ccb4f700d3a0f18286870b9f84
Cathy Hu <cahu@suse.de>
2021-04-26 14:46:18 +0200

    Add DBusServicesCheck

---
+++ rpmlint/checks/DBusServicesCheck.py
@@ -0,0 +1,29 @@
+from rpmlint.checks.AbstractCheck import AbstractCheck
+
+
+class DBusServicesCheck(AbstractCheck):
+    _dbus_system_paths = [
+        '/usr/share/dbus-1/system-services/',
+        '/usr/share/dbus-1/system.d/',
+        '/etc/dbus-1/system.d/'
+    ]
+
+    def __init__(self, config, output):
+        super().__init__(config, output)
+        self.services_whitelist = config.configuration['DBUSServices']['WhiteList']
+
+    def check(self, pkg):
+        if pkg.is_source:
+            return
+
+        for f in pkg.files:
+            for p in self._dbus_system_paths:
+                if f.startswith(p):
+
+                    if f in pkg.ghost_files:
+                        self.output.add_info('E', pkg, 'dbus-ghost-service', f)
+                        continue
+
+                    bn = f[len(p):]
+                    if bn not in self.services_whitelist:
+                        self.output.add_info('E', pkg, 'dbus-unauthorized-service', f)
