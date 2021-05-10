2292ada1f019a986cf14b834010cbade88936ff3
Cathy Hu <cahu@suse.de>
2021-05-10 08:25:42 +0200

    Refactor SUIDPermissionsCheck
    This removes suseconfig check, pie-binary check and refactors the check()
    function.

--- rpmlint/checks/SUIDPermissionsCheck.py
+++ rpmlint/checks/SUIDPermissionsCheck.py
@@ -23,6 +23,103 @@ class SUIDPermissionsCheck(AbstractCheck):
         parser = PermissionsParser(self.var_handler, path)
         self.perms.update(parser.getEntries())
 
+    def _check_restricted_mode(self, pkg, path, mode):
+        msg = (
+            '%(file)s is packaged with '
+            'setuid/setgid bits (0%(mode)o)'
+            % {'file': path, 'mode': stat.S_IMODE(mode)}
+        )
+        if not stat.S_ISDIR(mode):
+            self.output.add_info(
+                'E', pkg, 'permissions-file-setuid-bit', msg
+            )
+        else:
+            self.output.add_info(
+                'W', pkg, 'permissions-directory-setuid-bit', msg
+            )
+
+    def _verify_entry(self, pkg, path, mode, owner):
+        m = 0
+        o = 'invalid'
+        if stat.S_ISDIR(mode):
+            if path in self.perms:
+                self.output.add_info(
+                    'W', pkg, 'permissions-dir-without-slash', path
+                )
+            else:
+                path += '/'
+
+        entry = self.perms[path]
+
+        m = entry.mode
+        o = ':'.join((entry.owner, entry.group))
+
+        if stat.S_IMODE(mode) != m:
+            self.output.add_info(
+                'E',
+                pkg,
+                'permissions-incorrect',
+                '%(file)s has mode 0%(mode)o but should be 0%(m)o'
+                % {'file': path, 'mode': stat.S_IMODE(mode), 'm': m},
+            )
+
+        if owner != o:
+            self.output.add_info(
+                'E',
+                pkg,
+                'permissions-incorrect-owner',
+                '%(file)s belongs to %(owner)s but should be %(o)s'
+                % {'file': path, 'owner': owner, 'o': o},
+            )
+
+    def _check_post_scriptlets(self, pkg, path, need_verifyscript):
+        script = pkg[rpm.RPMTAG_POSTIN] or pkg.scriptprog(rpm.RPMTAG_POSTINPROG)
+        found = False
+        need_set_permissions = False
+
+        if script:
+            for line in script.split('\n'):
+                if 'chkstat -n' in line and path in line:
+                    found = True
+                    break
+
+        if need_verifyscript and (
+            path not in self.perms or not self._is_static_entry(self.perms[path])
+        ):
+
+            if not script or not found:
+                self.output.add_info(
+                    'E',
+                    pkg,
+                    'permissions-missing-postin',
+                    'missing %%set_permissions %s in %%post' % path,
+                )
+
+            need_set_permissions = True
+            script = (
+                pkg[rpm.RPMTAG_VERIFYSCRIPT] or pkg[rpm.RPMTAG_VERIFYSCRIPTPROG]
+            )
+
+            found = False
+            if script:
+                for line in script.split('\n'):
+                    if '/chkstat' in line and path in line:
+                        found = True
+                        break
+
+            if not script or not found:
+                self.output.add_info(
+                    'W',
+                    pkg,
+                    'permissions-missing-verifyscript',
+                    'missing %%verify_permissions -e %s' % path,
+                )
+
+        return need_set_permissions
+
+    def _exists_permissions_entry(self, path, mode):
+        return path in self.perms or (stat.S_ISDIR(mode) and path + '/' in self.perms)
+
     def _is_static_entry(self, entry):
         # entries coming from the fixed permissions profile are considered
         # static
@@ -45,7 +142,6 @@ class SUIDPermissionsCheck(AbstractCheck):
             return
 
         need_set_permissions = False
-        found_suseconfig = False
 
         for f, pkgfile in pkg.files.items():
 
@@ -62,59 +158,15 @@ class SUIDPermissionsCheck(AbstractCheck):
             owner = pkgfile.user + ':' + pkgfile.group
 
             need_verifyscript = False
-            if f in self.perms or (stat.S_ISDIR(mode) and f + '/' in self.perms):
+            if self._exists_permissions_entry(f, mode):
                 if stat.S_ISLNK(mode):
                     self.output.add_info('W', pkg, 'permissions-symlink', f)
                     continue
 
                 need_verifyscript = True
-
-                m = 0
-                o = 'invalid'
-                if stat.S_ISDIR(mode):
-                    if f in self.perms:
-                        self.output.add_info(
-                            'W', pkg, 'permissions-dir-without-slash', f
-                        )
-                    else:
-                        f += '/'
-
-                entry = self.perms[f]
-
-                if stat.S_ISREG(mode) and mode & (
-                    stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
-                ):
-                    # pie binaries have 'shared object' here
-                    if (
-                        pkgfile.magic.startswith('ELF ') and ('shared object' not in pkgfile.magic) and ('pie executable' not in pkgfile.magic)
-                    ):
-                        self.output.add_info(
-                            'E', pkg, 'non-position-independent-executable', f
-                        )
-
-                m = entry.mode
-                o = ':'.join((entry.owner, entry.group))
-
-                if stat.S_IMODE(mode) != m:
-                    self.output.add_info(
-                        'E',
-                        pkg,
-                        'permissions-incorrect',
-                        '%(file)s has mode 0%(mode)o but should be 0%(m)o'
-                        % {'file': f, 'mode': stat.S_IMODE(mode), 'm': m},
-                    )
-
-                if owner != o:
-                    self.output.add_info(
-                        'E',
-                        pkg,
-                        'permissions-incorrect-owner',
-                        '%(file)s belongs to %(owner)s but should be %(o)s'
-                        % {'file': f, 'owner': owner, 'o': o},
-                    )
+                self._verify_entry(pkg, f, mode, owner)
 
             elif not stat.S_ISLNK(mode):
-
                 if f + '/' in self.perms:
                     self.output.add_info(
                         'W',
@@ -125,74 +177,10 @@ class SUIDPermissionsCheck(AbstractCheck):
 
                 if mode & (stat.S_ISUID | stat.S_ISGID):
                     need_verifyscript = True
-                    msg = (
-                        '%(file)s is packaged with '
-                        'setuid/setgid bits (0%(mode)o)'
-                        % {'file': f, 'mode': stat.S_IMODE(mode)}
-                    )
-                    if not stat.S_ISDIR(mode):
-                        self.output.add_info(
-                            'E', pkg, 'permissions-file-setuid-bit', msg
-                        )
-                    else:
-                        self.output.add_info(
-                            'W', pkg, 'permissions-directory-setuid-bit', msg
-                        )
+                    self._check_restricted_mode(pkg, f, mode)
 
-                    if stat.S_ISREG(mode):
-                        if (
-                            'shared object' not in pkgfile.magic and 'pie executable' not in pkgfile.magic
-                        ):
-                            self.output.add_info(
-                                'E', pkg, 'non-position-independent-executable', f
-                            )
-
-            script = pkg[rpm.RPMTAG_POSTIN] or pkg.scriptprog(rpm.RPMTAG_POSTINPROG)
-            found = False
-            if script:
-                for line in script.split('\n'):
-                    if 'chkstat -n' in line and f in line:
-                        found = True
-                        break
-
-                    if (
-                        'SuSEconfig --module permissions' in line or 'run_permissions is obsolete' in line
-                    ):
-                        found = True
-                        found_suseconfig = True
-                        break
-
-            if need_verifyscript and (
-                f not in self.perms or not self._is_static_entry(self.perms[f])
-            ):
-
-                if not script or not found:
-                    self.output.add_info(
-                        'E',
-                        pkg,
-                        'permissions-missing-postin',
-                        'missing %%set_permissions %s in %%post' % f,
-                    )
-
+            if self._check_post_scriptlets(pkg, f, need_verifyscript):
                 need_set_permissions = True
-                script = (
-                    pkg[rpm.RPMTAG_VERIFYSCRIPT] or pkg[rpm.RPMTAG_VERIFYSCRIPTPROG]
-                )
-
-                found = False
-                if script:
-                    for line in script.split('\n'):
-                        if '/chkstat' in line and f in line:
-                            found = True
-                            break
-
-                if not script or not found:
-                    self.output.add_info(
-                        'W',
-                        pkg,
-                        'permissions-missing-verifyscript',
-                        'missing %%verify_permissions -e %s' % f,
-                    )
 
         if need_set_permissions:
             if 'permissions' not in map(lambda x: x[0], pkg.prereq):
@@ -202,11 +190,3 @@ class SUIDPermissionsCheck(AbstractCheck):
                     'permissions-missing-requires',
                     "missing 'permissions' in PreReq",
                 )
-
-        if found_suseconfig:
-            self.output.add_info(
-                'I',
-                pkg,
-                'permissions-suseconfig-obsolete',
-                '%run_permissions is obsolete',
-            )
