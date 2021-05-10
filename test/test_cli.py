86f962c54a3bb64ecdca6b68a456bf52fd7fa3b0
Cathy Hu <cahu@suse.de>
2021-05-10 11:44:14 +0200

    Adjust number of suse configs in test

--- test/test_cli.py
+++ test/test_cli.py
@@ -29,7 +29,7 @@ def test_parsing_config_file(test_arguments):
 def test_parsing_opensuse_conf(test_arguments):
     parsed = process_lint_args(test_arguments)
 
-    assert len(parsed['config']) == 11
+    assert len(parsed['config']) == 12
     assert PosixPath('configs/openSUSE/opensuse.toml') in parsed['config']
     assert PosixPath('configs/openSUSE/licenses.toml') in parsed['config']
     assert PosixPath('configs/openSUSE/pie-executables.toml') in parsed['config']
