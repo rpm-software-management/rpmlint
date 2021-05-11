4d21c5e0f58ae16671e780d522a206762b2987c4
Cathy Hu <cahu@suse.de>
2021-05-11 09:23:52 +0200

    Fix style issues

--- rpmlint/checks/SUIDPermissionsCheck.py
+++ rpmlint/checks/SUIDPermissionsCheck.py
@@ -7,6 +7,7 @@ from rpmlint.permissions import PermissionsParser, VariablesHandler
 
 
 class SUIDPermissionsCheck(AbstractCheck):
+
     def __init__(self, config, output):
         super().__init__(config, output)
         self.perms = {}
@@ -30,20 +31,14 @@ class SUIDPermissionsCheck(AbstractCheck):
             % {'file': path, 'mode': stat.S_IMODE(mode)}
         )
         if not stat.S_ISDIR(mode):
-            self.output.add_info(
-                'E', pkg, 'permissions-file-setuid-bit', msg
-            )
+            self.output.add_info('E', pkg, 'permissions-file-setuid-bit', msg)
         else:
-            self.output.add_info(
-                'W', pkg, 'permissions-directory-setuid-bit', msg
-            )
+            self.output.add_info('W', pkg, 'permissions-directory-setuid-bit', msg)
 
     def _verify_entry(self, pkg, path, mode, owner):
         if stat.S_ISDIR(mode):
             if path in self.perms:
-                self.output.add_info(
-                    'W', pkg, 'permissions-dir-without-slash', path
-                )
+                self.output.add_info('W', pkg, 'permissions-dir-without-slash', path)
             else:
                 path += '/'
 
@@ -53,22 +48,12 @@ class SUIDPermissionsCheck(AbstractCheck):
         o = ':'.join((entry.owner, entry.group))
 
         if stat.S_IMODE(mode) != m:
-            self.output.add_info(
-                'E',
-                pkg,
-                'permissions-incorrect',
-                '%(file)s has mode 0%(mode)o but should be 0%(m)o'
-                % {'file': path, 'mode': stat.S_IMODE(mode), 'm': m},
-            )
+            self.output.add_info('E', pkg, 'permissions-incorrect',
+                                 '%(file)s has mode 0%(mode)o but should be 0%(m)o' % {'file': path, 'mode': stat.S_IMODE(mode), 'm': m})
 
         if owner != o:
-            self.output.add_info(
-                'E',
-                pkg,
-                'permissions-incorrect-owner',
-                '%(file)s belongs to %(owner)s but should be %(o)s'
-                % {'file': path, 'owner': owner, 'o': o},
-            )
+            self.output.add_info('E', pkg, 'permissions-incorrect-owner',
+                                 '%(file)s belongs to %(owner)s but should be %(o)s' % {'file': path, 'owner': owner, 'o': o})
 
     def _check_post_scriptlets(self, pkg, path, need_verifyscript):
         script = pkg[rpm.RPMTAG_POSTIN] or pkg.scriptprog(rpm.RPMTAG_POSTINPROG)
@@ -84,19 +69,12 @@ class SUIDPermissionsCheck(AbstractCheck):
         if need_verifyscript and (
             path not in self.perms or not self._is_static_entry(self.perms[path])
         ):
-
             if not script or not found:
-                self.output.add_info(
-                    'E',
-                    pkg,
-                    'permissions-missing-postin',
-                    'missing %%set_permissions %s in %%post' % path,
-                )
+                self.output.add_info('E', pkg, 'permissions-missing-postin',
+                                     'missing %%set_permissions %s in %%post' % path)
 
             need_set_permissions = True
-            script = (
-                pkg[rpm.RPMTAG_VERIFYSCRIPT] or pkg[rpm.RPMTAG_VERIFYSCRIPTPROG]
-            )
+            script = (pkg[rpm.RPMTAG_VERIFYSCRIPT] or pkg[rpm.RPMTAG_VERIFYSCRIPTPROG])
 
             found = False
             if script:
@@ -106,12 +84,8 @@ class SUIDPermissionsCheck(AbstractCheck):
                         break
 
             if not script or not found:
-                self.output.add_info(
-                    'W',
-                    pkg,
-                    'permissions-missing-verifyscript',
-                    'missing %%verify_permissions -e %s' % path,
-                )
+                self.output.add_info('W', pkg, 'permissions-missing-verifyscript',
+                                     'missing %%verify_permissions -e %s' % path)
 
         return need_set_permissions
 
@@ -142,15 +116,9 @@ class SUIDPermissionsCheck(AbstractCheck):
         need_set_permissions = False
 
         for f, pkgfile in pkg.files.items():
-
             if pkgfile.filecaps:
-                self.output.add_info(
-                    'E',
-                    pkg,
-                    'permissions-fscaps',
-                    "%(fname)s has fscaps '%(caps)s'"
-                    % {'fname': f, 'caps': pkgfile.filecaps},
-                )
+                self.output.add_info('E', pkg, 'permissions-fscaps',
+                                     "%(fname)s has fscaps '%(caps)s'" % {'fname': f, 'caps': pkgfile.filecaps})
 
             mode = pkgfile.mode
             owner = pkgfile.user + ':' + pkgfile.group
@@ -166,12 +134,7 @@ class SUIDPermissionsCheck(AbstractCheck):
 
             elif not stat.S_ISLNK(mode):
                 if f + '/' in self.perms:
-                    self.output.add_info(
-                        'W',
-                        pkg,
-                        'permissions-file-as-dir',
-                        f + ' is a file but listed as directory',
-                    )
+                    self.output.add_info('W', pkg, 'permissions-file-as-dir', f + ' is a file but listed as directory')
 
                 if mode & (stat.S_ISUID | stat.S_ISGID):
                     need_verifyscript = True
@@ -182,9 +145,4 @@ class SUIDPermissionsCheck(AbstractCheck):
 
         if need_set_permissions:
             if 'permissions' not in map(lambda x: x[0], pkg.prereq):
-                self.output.add_info(
-                    'E',
-                    pkg,
-                    'permissions-missing-requires',
-                    "missing 'permissions' in PreReq",
-                )
+                self.output.add_info('E', pkg, 'permissions-missing-requires', "missing 'permissions' in PreReq")
