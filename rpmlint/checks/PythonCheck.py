731065974ada69fbb3fa563e54ce86886749c454
Daniel Garcia Moreno <daniel.garcia@suse.com>
2022-11-28 08:47:27 +0100

    PythonCheck: Allow tests folder in python packages
    Fix https://github.com/rpm-software-management/rpmlint/issues/965

--- rpmlint/checks/PythonCheck.py
+++ rpmlint/checks/PythonCheck.py
@@ -14,7 +14,6 @@ ERRS = {
     'tests': 'python-tests-in-site-packages',
     'doc': 'python-doc-in-site-packages',
     'src': 'python-src-in-site-packages',
-    'tests-package': 'python-tests-in-package',
 }
 
 SITELIB_RE = '/usr/lib[^/]*/python[^/]*/site-packages'
@@ -25,7 +24,6 @@ ERR_PATHS = [
     (re.compile(f'{SITELIB_RE}/tests?$'), 'tests'),
     (re.compile(f'{SITELIB_RE}/docs?$'), 'doc'),
     (re.compile(f'{SITELIB_RE}/src$'), 'src'),
-    (re.compile(f'{SITELIB_RE}/.+/tests?$'), 'tests-package'),
 ]
 
 # Paths that shouldn't be in any packages, but might need to be under
@@ -57,11 +55,6 @@ class PythonCheck(AbstractFilesCheck):
 
         for path_re, key in ERR_PATHS:
             if path_re.match(filename):
-                if key == 'tests-package':
-                    # Ignore "-test" and "-tests" packages since these are
-                    # supposed to contain tests.
-                    if pkg.name.endswith(('test', 'tests')):
-                        continue
                 self.output.add_info('E', pkg, ERRS[key], filename)
 
     def check_egginfo(self, pkg, filename):
