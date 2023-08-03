e15bb90c4b416211d94518bdd45fa19e4c58b6f8
afrid18 <afridhussain.d@gmail.com>
2023-08-03 09:34:23 +0000

    test: mocked test_duplicates tests
    added a new helper method in FakePkg class which
    is  initiate_files_base_data, this is used to initiate
    fakepkg class with basic files data after setting
    up custom metadata;
    removed a binary
    other binary is required for  file

--- test/Testing.py
+++ test/Testing.py
@@ -68,4 +68,5 @@ def get_tested_mock_package(files=None, real_files=None, header=None):
         mockPkg.create_files(files, real_files)
     if header is not None:
         mockPkg.add_header(header)
+    mockPkg.initiate_files_base_data()
     return mockPkg
