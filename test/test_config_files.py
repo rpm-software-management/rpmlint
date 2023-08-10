8bfaed185c85d726166f1aa04a6ef96e9b1ce417
afrid18 <afridhussain.d@gmail.com>
2023-08-10 08:42:13 +0000

    test: mocked test_config_files
    There are total 2 tests and 2 binaries
    one binary test/binary/config-files-1.0-0.x86_64.rpm
    is removed and another binary test/binary/logrotate-0-0.x86_64.rpm
    is used by test/test_logrotate.py and hence it isn't removed

--- test/test_config_files.py
+++ test/test_config_files.py
@@ -2,7 +2,7 @@ import pytest
 from rpmlint.checks.ConfigFilesCheck import ConfigFilesCheck
 from rpmlint.filter import Filter
 
-from Testing import CONFIG, get_tested_package
+from Testing import CONFIG, get_tested_mock_package
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -13,10 +13,16 @@ def configfilescheck():
     return output, test
 
 
-@pytest.mark.parametrize('package', ['binary/config-files'])
-def test_config_files(tmp_path, package, configfilescheck):
+@pytest.mark.parametrize('package', [get_tested_mock_package(
+    files={
+        '/etc/conffile1': {'content': '# Conffile1', 'metadata': {'flags': 1}},
+        '/var/conffile2': {'content': '# Conffile2', 'metadata': {'flags': 1}},
+        '/usr/share/conffile3': {'content': '# Conffile3', 'metadata': {'flags': 1}},
+    }
+)])
+def test_config_files1(package, configfilescheck):
     output, test = configfilescheck
-    test.check(get_tested_package(package, tmp_path))
+    test.check(package)
     out = output.print_results(output.results)
     assert 'non-etc-or-var-file-marked-as-conffile /usr/share/conffile3' in out
     assert 'conffile-without-noreplace-flag /etc/conffile1' in out
@@ -24,10 +30,19 @@ def test_config_files(tmp_path, package, configfilescheck):
     assert 'conffile-without-noreplace-flag /usr/share/conffile3' in out
 
 
-@pytest.mark.parametrize('package', ['binary/logrotate'])
-def test_config_files_correct(tmp_path, package, configfilescheck):
+@pytest.mark.parametrize('package', [get_tested_mock_package(
+    files={
+        'tmp/foo/my.log': {'content': '', 'metadata': {'flags': 0}},
+        'tmp/foo2': {'content': '', 'metadata': {'flags': 0}},
+        'tmp/foo2/my.log': {'content': '', 'metadata': {'flags': 0}},
+        'etc/logrotate.d': {'content': '', 'metadata': {'flags': 0}},
+        'etc/logrotate.d/logrotate2.conf': {'content': '', 'metadata': {'flags': 0}},
+        'etc/logrotate.d/logrotate.conf': {'content': '', 'metadata': {'flags': 0}},
+    }
+)])
+def test_config_files_correct1(package, configfilescheck):
     output, test = configfilescheck
-    test.check(get_tested_package(package, tmp_path))
+    test.check(package)
     out = output.print_results(output.results)
     assert 'non-etc-or-var-file-marked-as-conffile' not in out
     assert 'conffile-without-noreplace-flag' not in out
