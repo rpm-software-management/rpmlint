b95c28c3214f379755c19dc6da359de4a48f0f5f
Luz Marina Montilla Marín <luzmarina@localhost.localdomain>
2024-01-30 15:55:52 +0100

    Clean tests file

--- test/test_python.py
+++ test/test_python.py
@@ -4,6 +4,19 @@ from rpmlint.filter import Filter
 
 from Testing import CONFIG, get_tested_mock_package
 
+from mock_packages import( 
+    PythonSitePackage,
+    PythonModuleMockPackage,
+    PythonDistutilsMockPackage,
+    PythonFlitMockPackage,
+    PythonJupyterServerFileidMockPackage,
+    PythonJupyterEventsMockPackage,
+    PythonScikitBuildMockPackage,
+    PythonFlitUpdatedMockPackage,
+    PythonIcecreamPackage,
+    PythonBlinkerMockPackage,
+    PythonSiteBlinkerMockPackage,
+)
 
 @pytest.fixture(scope='function', autouse=True)
 def pythoncheck():
@@ -25,18 +38,7 @@ def test(pythoncheck):
     yield test
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files=[
-        '/usr/lib/python2.7/site-packages/python-mypackage/doc',
-        '/usr/lib/python2.7/site-packages/python-mypackage/docs',
-        '/usr/lib/python3.10/site-packages/python-mypackage/doc',
-        '/usr/lib/python3.10/site-packages/python-mypackage/docs',
-        '/usr/lib64/python2.7/site-packages/python-mypackage/doc',
-        '/usr/lib64/python2.7/site-packages/python-mypackage/docs',
-        '/usr/lib64/python3.10/site-packages/python-mypackage/doc',
-        '/usr/lib64/python3.10/site-packages/python-mypackage/docs',
-    ]
-)])
+@pytest.mark.parametrize('package', [PythonSitePackage])
 def test_python_doc_in_package(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
@@ -50,18 +52,7 @@ def test_python_doc_in_package(package, test, output):
     assert 'W: python-doc-in-package /usr/lib64/python3.10/site-packages/python-mypackage/docs' in out
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files={
-        '/usr/lib/python2.7/site-packages/python-mypackage/doc/__init__.py': {'create_dirs': True, 'include_dirs': 2},
-        '/usr/lib/python2.7/site-packages/python-mypackage/docs/__init__.py': {'create_dirs': True, 'include_dirs': 1},
-        '/usr/lib64/python2.7/site-packages/python-mypackage/doc/__init__.py': {'create_dirs': True, 'include_dirs': 2},
-        '/usr/lib64/python2.7/site-packages/python-mypackage/docs/__init__.py': {'create_dirs': True, 'include_dirs': 1},
-        '/usr/lib/python3.10/site-packages/python-mypackage/doc/__init__.py': {'create_dirs': True, 'include_dirs': 2},
-        '/usr/lib/python3.10/site-packages/python-mypackage/docs/__init__.py': {'create_dirs': True, 'include_dirs': 1},
-        '/usr/lib64/python3.10/site-packages/python-mypackage/doc/__init__.py': {'create_dirs': True, 'include_dirs': 2},
-        '/usr/lib64/python3.10/site-packages/python-mypackage/docs/__init__.py': {'create_dirs': True, 'include_dirs': 1},
-    }
-)])
+@pytest.mark.parametrize('package', [PythonModuleMockPackage])
 def test_python_doc_module_in_package(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
@@ -75,14 +66,7 @@ def test_python_doc_module_in_package(package, test, output):
     assert 'W: python-doc-in-package /usr/lib64/python3.10/site-packages/python-mypackage/docs' not in out
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files={
-        '/usr/lib/python2.7/site-packages/mydistutilspackage.egg-info': {'content': 'Metadata-Version: 2.1\nName: pythoncheck'},
-        '/usr/lib/python3.10/site-packages/mydistutilspackage.egg-info': {'content': 'Metadata-Version: 2.1\nName: pythoncheck'},
-        '/usr/lib64/python2.7/site-packages/mydistutilspackage.egg-info': {'content': 'Metadata-Version: 2.1\nName: pythoncheck'},
-        '/usr/lib64/python3.10/site-packages/mydistutilspackage.egg-info': {'content': 'Metadata-Version: 2.1\nName: pythoncheck'},
-    },
-)])
+@pytest.mark.parametrize('package', [PythonDistutilsMockPackage])
 def test_python_distutils_egg_info(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
@@ -159,103 +143,11 @@ def test_python_tests_in_site_packages(package, test, output):
     assert 'E: python-tests-in-site-packages /usr/lib64/python3.10/site-packages/tests' in out
 
 
-@pytest.mark.parametrize('package', [
-    get_tested_mock_package(
-        files={
-            '/usr/lib/python3.10/site-packages/flit-3.8.0.dist-info/METADATA': {
-                'content-path': 'files/python-flit-metadata.txt',
-                'create_dirs': True,
-            },
-        },
-        header={
-            'requires': [
-                'python-flit_core',
-                'python-requests',
-                'python-tomli-w',
-                'python310-docutils',
-            ],
-        },
-    ),
-    get_tested_mock_package(
-        files={
-            '/usr/lib/python3.10/site-packages/jupyter_server_fileid-0.9.0.dist-info/METADATA': {
-                'content-path': 'files/python-jupyter_server_fileid-metadata.txt',
-                'create_dirs': True
-            },
-        },
-        header={
-            'requires': [
-                'python-jupyter-events',
-                'python-click',
-                'python-jupyter-server',
-            ],
-        },
-    ),
-    get_tested_mock_package(
-        files={
-            '/usr/lib/python3.10/site-packages/jupyter_events-0.6.3.dist-info/METADATA': {
-                'content-path': 'files/python-jupyter-events-metadata.txt',
-                'create_dirs': True
-            },
-        },
-        header={
-            'requires': [
-                'python-jsonschema',
-                'python-python-json-logger',
-                'python-pyyaml',
-                'python-rfc3339-validator',
-                'python-rfc3986-validator',
-                'python-traitlets',
-                'python-click',
-                'python-rich',
-                'python-jupyterlite-sphinx',
-                'python-myst-parser',
-                'python-pydata-sphinx-theme',
-                'python-sphinxcontrib-spelling',
-                'python-click',
-                'python-coverage',
-                'python-pre-commit',
-                'python-pytest-asyncio',
-                'python-pytest-console-scripts',
-                'python-pytest-cov',
-                'python-pytest',
-                'python-rich',
-            ],
-        },
-    ),
-    get_tested_mock_package(
-        files={
-            '/usr/lib/python3.10/site-packages/scikit_build-0.17.2.dist-info/METADATA': {
-                'content-path': 'files/python-scikit_build-metadata.txt',
-                'create_dirs': True
-            },
-        },
-        header={
-            'requires': [
-                'python-distro',
-                'python-packaging',
-                'python-setuptools',
-                'python-wheel',
-                'python-tomli',
-            ],
-        },
-    ),
-    get_tested_mock_package(
-        files={
-            '/usr/lib/python3.10/site-packages/flit-3.8.0.dist-info/METADATA': {
-                'content-path': 'files/python-flit-metadata.txt',
-                'create_dirs': True,
-            },
-        },
-        header={
-            'requires': [
-                'python3.12dist(docutils)',
-                'python3.12dist(flit-core) >= 3.8',
-                'python3.12dist(requests)',
-                'python3.12dist(tomli-w)',
-            ],
-        },
-    ),
+@pytest.mark.parametrize('package', [PythonFlitMockPackage,
+    PythonJupyterServerFileidMockPackage,
+    PythonJupyterEventsMockPackage,
+    PythonScikitBuildMockPackage,
+    PythonFlitUpdatedMockPackage,
 ])
 def test_python_dependencies_metadata(package, test, output):
     test.check(package)
@@ -264,27 +156,7 @@ def test_python_dependencies_metadata(package, test, output):
     assert 'W: python-leftover-require' not in out
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files={
-        '/usr/lib/python3.10/site-packages/icecream-2.1.3-py3.10.egg-info/requires.txt': {
-            'content': """
-asttokens>=2.0.1
-colorama>=0.3.9
-executing>=0.3.1
-pygments>=2.2.0
-""",
-            'create_dirs': True
-        },
-    },
-    header={
-        'requires': [
-            'asttokens>=2.0.1',
-            'colorama>=0.3.9',
-            'executing>=0.3.1',
-            'pygments>=2.2.0',
-        ],
-    },
-)])
+@pytest.mark.parametrize('package', [PythonIcecreamPackage])
 def test_python_dependencies_requires(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
@@ -386,40 +258,14 @@ def test_python_dependencies_leftover(package, test, output):
     assert 'W: python-leftover-require' in out
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files=[
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/base.cpython-310.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/base.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/base.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/__init__.cpython-310.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/__init__.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/__init__.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_saferef.cpython-310.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_saferef.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_saferef.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_utilities.cpython-310.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_utilities.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_utilities.cpython-39.pyc',
-    ]
-)])
+@pytest.mark.parametrize('package', [PythonBlinkerMockPackage])
 def test_python_pyc_multiple_versions(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
     assert 'W: python-pyc-multiple-versions expected: 310' in out
 
 
-@pytest.mark.parametrize('package', [get_tested_mock_package(
-    files=[
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/base.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/base.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/__init__.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/__init__.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_saferef.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_saferef.cpython-39.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_utilities.cpython-39.opt-1.pyc',
-        '/usr/lib/python3.9/site-packages/blinker/__pycache__/_utilities.cpython-39.pyc',
-    ]
-)])
+@pytest.mark.parametrize('package', [PythonSiteBlinkerMockPackage])
 def test_python_pyc_single_version(package, test, output):
     test.check(package)
     out = output.print_results(output.results)
