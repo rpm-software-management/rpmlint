fa41771b36fceb8d5f84e1a0c1564dd643050381
Luz Marina <i12momal@uco.es>
2024-07-11 11:48:46 +0200

    Fix for bug detected in test_appdata.py
    The files are placed in the test/files directory

--- test/mockdata/mock_appdata.py
+++ test/mockdata/mock_appdata.py
@@ -2,13 +2,17 @@ from Testing import get_tested_mock_package
 
 APPDATA = get_tested_mock_package(
 files={
-'/usr/share/appdata/broken.appdata.xml': {'content_path': '/usr/share/appdata/broken.appdata.xml'},
-'/usr/share/appdata/terminator.appdata.xml': {'content_path': '/usr/share/appdata/terminator.appdata.xml'}
-}
-)
+'/usr/share/appdata/broken.appdata.xml': {
+    'content-path': 'files/broken.appdata.xml',
+    'create_dirs': True},
+'/usr/share/appdata/broken-xml.metainfo.xml': {
+    'content-path': 'files/broken-xml.metainfo.xml',
+    'create_dirs': True}
+})
 
 APPDATA2 = get_tested_mock_package(
 files={
-'/usr/share/appdata/terminator.appdata.xml': {'content_path': '/usr/share/appdata/terminator.appdata.xml'}
-}
-)
+'/usr/share/appdata/broken-xml.metainfo.xml': {
+    'content-path': 'files/broken-xml.metainfo.xml',
+    'create_dirs': True}
+})
