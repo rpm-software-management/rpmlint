4805966051da3872f8603e2354ed2655afd10078
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-09-20 13:54:08 +0200

    test: Replace logrotate rpm with mock
    See https://github.com/rpm-software-management/rpmlint/issues/1105

--- test/mockdata/mock_logrotate.py
+++ test/mockdata/mock_logrotate.py
@@ -1,3 +1,5 @@
+import stat
+
 from rpm import RPMFILE_CONFIG, RPMFILE_NOREPLACE
 
 from Testing import get_tested_mock_package
@@ -6,9 +8,45 @@ from Testing import get_tested_mock_package
 LogrotatePackage = get_tested_mock_package(
     lazyload=True,
     files={
-        'tmp/foo/my.log': {},
-        'tmp/foo2/my.log': {},
-        'etc/logrotate.d/logrotate.conf': { 'content-path': 'files/logrotate/logrotate.conf' },
-        'etc/logrotate.d/logrotate2.conf': { 'content-path': 'files/logrotate/logrotate2.conf' },
+        '/tmp/foo': {
+            'create_dirs': True,
+            'is_dir': True,
+            'metadata': {
+                'mode': stat.S_IFDIR | 0o755,
+                'user': 'marxin',
+                'group': 'users',
+            },
+        },
+        '/tmp/foo2': {
+            'create_dirs': True,
+            'is_dir': True,
+            'metadata': {
+                'mode': stat.S_IFREG | stat.S_ISUID | 0o777,
+                'user': 'root',
+                'group': 'users2',
+            },
+        },
+        '/tmp/foo/my.log': {
+            'metadata': {
+                'mode': 0o644 | stat.S_IFREG,
+                'user': 'root',
+                'group': 'users2',
+            },
+        },
+        '/tmp/foo2/my.log': {
+            'metadata': {
+                'mode': stat.S_IFREG | stat.S_ISUID | 0o777,
+                'user': 'root',
+                'group': 'users2',
+            },
+        },
+        '/etc/logrotate.d/logrotate.conf': {
+            'create_dirs': True,
+            'content-path': 'files/logrotate/logrotate.conf',
+        },
+        '/etc/logrotate.d/logrotate2.conf': {
+            'create_dirs': True,
+            'content-path': 'files/logrotate/logrotate2.conf',
+        },
     }
 )
