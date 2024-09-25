b3385a6776d254275c5449858fedfafdfb5a534a
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-09-25 16:51:32 +0200

    test: Normalize mock_zypp_syntax

--- test/mockdata/mock_zypp_syntax.py
+++ test/mockdata/mock_zypp_syntax.py
@@ -1,14 +1,10 @@
 from Testing import get_tested_mock_package
 
-ZYPPSYNTAX = get_tested_mock_package(
+
+SyntaxPackageandPackage = get_tested_mock_package(
+    lazyload=True,
     header={
-        'requires': [
-            'rpmlib(CompressedFileNames) <= 3.0.4-1',
-            'rpmlib(FileDigests) <= 4.6.0-1',
-            'rpmlib(PayloadFilesHavePrefix) <= 4.0-1',
-            'rpmlib(PayloadIsXz) <= 5.2-1',
-            'rpmlib(RichDependencies) <= 4.12.0-1',
-        ],
+        'requires': [],
         'supplements': ['packageand(c:d)'],
         'recommends': ['packageand(a:b)'],
         'suggests': ['(a and b)'],
@@ -19,15 +15,11 @@ ZYPPSYNTAX = get_tested_mock_package(
     },
 )
 
-ZYPPSYNTAX2 = get_tested_mock_package(
+
+SyntaxAndPackage = get_tested_mock_package(
+    lazyload=True,
     header={
-        'requires': [
-            'rpmlib(CompressedFileNames) <= 3.0.4-1',
-            'rpmlib(FileDigests) <= 4.6.0-1',
-            'rpmlib(PayloadFilesHavePrefix) <= 4.0-1',
-            'rpmlib(PayloadIsXz) <= 5.2-1',
-            'rpmlib(RichDependencies) <= 4.12.0-1',
-        ],
+        'requires': [],
         'supplements': ['(c and d)'],
         'recommends': ['b'],
         'suggests': ['(a and b)'],
