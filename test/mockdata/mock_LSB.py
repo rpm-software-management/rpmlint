2bed8e9c15a5b960434fffba3027eedf6dd595e5
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-09-25 13:33:24 +0200

    test: Normalize mock_LSB

--- test/mockdata/mock_LSB.py
+++ test/mockdata/mock_LSB.py
@@ -1,17 +1,20 @@
 from Testing import get_tested_mock_package
 
 
-LSB = get_tested_mock_package(
-files={
-'/usr/sbin/fping',
-'/usr/share/doc/packages/fPing',
-'/usr/share/doc/packages/fPing/CHANGELOG.md',
-'/usr/share/licenses/fPing',
-'/usr/share/licenses/fPing/COPYING',
-'/usr/share/man/man8/fping.8.gz',
-},
-header={
-'name': 'fPing@1',
-'version': '1.0_beta',
-'release': '1.0-beta'
-})
+NonLSBCompliancePackage = get_tested_mock_package(
+    lazyload=True,
+    name='fPing@1',
+    files={
+        '/usr/sbin/fping',
+        '/usr/share/doc/packages/fPing',
+        '/usr/share/doc/packages/fPing/CHANGELOG.md',
+        '/usr/share/licenses/fPing',
+        '/usr/share/licenses/fPing/COPYING',
+        '/usr/share/man/man8/fping.8.gz',
+    },
+    header={
+        'name': 'fPing@1',
+        'version': '1.0_beta',
+        'release': '1.0-beta'
+    },
+)
