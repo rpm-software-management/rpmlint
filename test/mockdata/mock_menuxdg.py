5a4757a59d885ea1ab65c9ba28b1756bdd470312
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-09-25 13:46:31 +0200

    test: Normalize mock_menuxdg

--- test/mockdata/mock_menuxdg.py
+++ test/mockdata/mock_menuxdg.py
@@ -1,17 +1,13 @@
 from Testing import get_tested_mock_package
 
-MENUXDG = get_tested_mock_package(
-files={
-'/usr/share/applications/somefile.desktop': {'content': """
+
+BROKEN = """
 [DEFAULT]
 something = true
 [else
-"""}
-})
+"""
 
-MENUXDG1 = get_tested_mock_package(
-files={
-'/usr/share/applications/rpmlint-test.desktop': {'content': """
+BADBIN = """
 [Desktop Entry]
 Name=rpmlint-test
 Exec=rpmlint-test file.file
@@ -19,12 +15,10 @@ Icon=chameleon_v_balíku
 Type=Application
 GenericName=rpmlint testcase
 Categories=Game;Amusement;
-"""}
-})
+"""
 
-MENUXDG2 = get_tested_mock_package(
-files={
-'/usr/share/applications/rpmlint-test.desktop': {'content': """
+
+BADDUP = """
 [Desktop Entry]
 [Desktop Entry]
 Name=rpmlint-test
@@ -34,36 +28,61 @@ Icon=chameleon_v_balíku
 Type=Application
 GenericName=rpmlint testcase
 Categories=Game;Amusement;
-"""}
-})
+"""
 
-MENUXDG3 = get_tested_mock_package(
-files={
-'/usr/share/applications/rpmlint-test.desktop': {'content': """
+
+BADSEC = """
 Name=rpmlint-test
 [Bad Section]
 Name[x-test]=xxrpmlint-testxx
 Exec=rpmlint-test file.file
 Icon=rpmlint-test
 Type=Application
-GenericName=rpmlint testcase"""}
-})
+GenericName=rpmlint testcase
+"""
 
-MENUXDG4 = get_tested_mock_package(
-files={
-'/usr/share/applications/rpmlint-test.desktop': {
-    'content-path': 'files/rpmlint-test.desktop',
-    'create_dirs': True,
-}
-})
 
-MENUXDG5 = get_tested_mock_package(
-files={
-'/usr/bin/rpmlint-test',
-'/usr/share/applications',
-'/usr/share/applications/rpmlint-test.desktop',
-'/usr/share/icons/hicolor',
-'/usr/share/icons/hicolor/scalable',
-'/usr/share/icons/hicolor/scalable/apps',
-'/usr/share/icons/hicolor/scalable/apps/chameleon_v_balíku.png'
-})
+MenuXDGInvalidPackage = get_tested_mock_package(
+    lazyload=True,
+    files={'/usr/share/applications/somefile.desktop': {'content': BROKEN}},
+)
+
+
+MenuXDGBadBinPackage = get_tested_mock_package(
+    lazyload=True,
+    files={'/usr/share/applications/rpmlint-test.desktop': {'content': BADBIN}},
+)
+
+
+MenuXDGBadDupPackage = get_tested_mock_package(
+    lazyload=True,
+    files={'/usr/share/applications/rpmlint-test.desktop': {'content': BADDUP}},
+)
+
+
+MenuXDGBadSecPackage = get_tested_mock_package(
+    lazyload=True,
+    files={'/usr/share/applications/rpmlint-test.desktop': {'content': BADSEC}},
+)
+
+
+MenuXDGBadUTF8Package = get_tested_mock_package(
+    lazyload=True,
+    files={
+        '/usr/share/applications/rpmlint-test.desktop': {
+            'content-path': 'files/rpmlint-test.desktop',
+            'create_dirs': True,
+        },
+    },
+)
+
+MenuXDGPackage = get_tested_mock_package(
+    lazyload=True,
+    files={
+        '/usr/bin/rpmlint-test': {},
+        '/usr/share/applications/rpmlint-test.desktop': {
+            'content-path': 'files/rpmlint-test-good.desktop',
+            'create_dirs': True,
+        },
+    },
+)
